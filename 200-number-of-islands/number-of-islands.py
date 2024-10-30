ec48d148e0cebe63fe324de50a6dd754a391141c
malhar07 <43933328+malhar07@users.noreply.github.com>
2024-10-30 01:52:05 -0400

    Time: 297 ms (17.13%) | Memory: 24.4 MB (27.29%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -4,19 +4,15 @@ class Solution:
         islands = 0
 
         def dfs(r,c):
-            q = deque()
-            q.append((r,c))
             visited.add((r,c))
 
             directions = [[-1,0], [1,0], [0,-1], [0,1]]
-
-            while q:
-                r,c = q.popleft()
-                for dr, dc in directions:
+            for dr, dc in directions:
                     if r+dr < 0 or r+dr >=len(grid) or c+dc < 0 or c+dc >= len(grid[0]) or (r+dr,c+dc) in visited or grid[r+dr][c+dc]!="1":
                         continue
-                    q.append((r+dr, c+dc))
-                    visited.add((r+dr, c+dc))
+                    visited.add((r+dr,c+dc))
+                    dfs(r+dr, c+dc)
+
 
         for r in range(len(grid)):
             for c in range(len(grid[0])):
