d26448b571c3dd80743e94bac2f0fa396a69abd9
malhar07 <43933328+malhar07@users.noreply.github.com>
2024-11-13 19:02:40 -0500

    Time: 5 ms (33.03%) | Memory: 20.2 MB (50.33%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.py
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.py
@@ -1,35 +1,65 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# # Definition for a binary tree node.
+# # class TreeNode:
+# #     def __init__(self, val=0, left=None, right=None):
+# #         self.val = val
+# #         self.left = left
+# #         self.right = right
+# class Solution:
+#     def findMode(self, root: Optional[TreeNode]) -> List[int]:
+#         self.arr = [] # to store inorder traversal
+#         count = 0
+#         self.max_count = 0
+
+#         def helper(root, c, prev):
+#             if root == None:
+#                 return c, prev
+#             count = c
+#             last_node = prev
+
+#             count, last_node = helper(root.left, count, last_node)
+
+#             if last_node == None or root.val == last_node:
+#                 count+1
+#                 helper(root.right, count, root.val)
+#             else:
+#                 if count > self.max_count:
+#                     self.max_count = count
+#                     self.arr = [last_node]
+#                 elif count == self.max_count:
+#                     self.arr.append(last_node)
+#                 helper(root.right, 1, root.val)
+#             return [count, last_node]
+#         prev = None
+#         helper(root, 0, prev)
+#         return self.arr
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
-        arr = [] # to store inorder traversal
-
-        def helper(root):
-            if root == None:
-                return 
-            helper(root.left)
-            arr.append(root.val)
-            helper(root.right)
-        helper(root)
-        print(arr)
-
-        count = 1
-        max_count = 0
-        res = []
-        for ind, i in enumerate(arr):
-            if ind == len(arr)-1 or arr[ind]!=arr[ind+1]:
-                if count == max_count:
-                    res.append(i)
-                elif count> max_count:
-                    max_count = count
-                    res = [i]
-                count=1
+        def dfs(node):
+            nonlocal max_streak, curr_streak, curr_num, ans
+            if not node:
+                return
+            
+            dfs(node.left)
+            
+            num = node.val
+            if num == curr_num:
+                curr_streak += 1
             else:
-                count+=1
-        return res
+                curr_streak = 1
+                curr_num = num
 
-[1, 2]
+            if curr_streak > max_streak:
+                ans = []
+                max_streak = curr_streak
+
+            if curr_streak == max_streak:
+                ans.append(num)
+            
+            dfs(node.right)
+
+        max_streak = 0
+        curr_streak = 0
+        curr_num = 0
+        ans = []
+        dfs(root)
+        return ans
