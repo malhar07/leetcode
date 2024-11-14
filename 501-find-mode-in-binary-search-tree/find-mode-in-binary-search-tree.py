9dc2be6eeffd58b22bd8c8c0d8d3a48bf89993db
malhar07 <43933328+malhar07@users.noreply.github.com>
2024-11-13 19:33:48 -0500

    Time: 1 ms (91.22%) | Memory: 20.3 MB (44.32%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.py
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.py
@@ -1,65 +1,36 @@
-# # Definition for a binary tree node.
-# # class TreeNode:
-# #     def __init__(self, val=0, left=None, right=None):
-# #         self.val = val
-# #         self.left = left
-# #         self.right = right
-# class Solution:
-#     def findMode(self, root: Optional[TreeNode]) -> List[int]:
-#         self.arr = [] # to store inorder traversal
-#         count = 0
-#         self.max_count = 0
-
-#         def helper(root, c, prev):
-#             if root == None:
-#                 return c, prev
-#             count = c
-#             last_node = prev
-
-#             count, last_node = helper(root.left, count, last_node)
-
-#             if last_node == None or root.val == last_node:
-#                 count+1
-#                 helper(root.right, count, root.val)
-#             else:
-#                 if count > self.max_count:
-#                     self.max_count = count
-#                     self.arr = [last_node]
-#                 elif count == self.max_count:
-#                     self.arr.append(last_node)
-#                 helper(root.right, 1, root.val)
-#             return [count, last_node]
-#         prev = None
-#         helper(root, 0, prev)
-#         return self.arr
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def findMode(self, root: Optional[TreeNode]) -> List[int]:
-        def dfs(node):
-            nonlocal max_streak, curr_streak, curr_num, ans
-            if not node:
+        self.arr = [] # to store inorder traversal
+        self.count = 0
+        self.max_count = 0
+
+        def helper(root):
+            if root == None:
                 return
-            
-            dfs(node.left)
-            
-            num = node.val
-            if num == curr_num:
-                curr_streak += 1
+
+            helper(root.left)
+
+            if root.val == self.prev:
+                self.count+=1
             else:
-                curr_streak = 1
-                curr_num = num
+                self.prev = root.val
+                self.count = 1
 
-            if curr_streak > max_streak:
-                ans = []
-                max_streak = curr_streak
-
-            if curr_streak == max_streak:
-                ans.append(num)
+            if self.count > self.max_count:
+                self.max_count = self.count
+                self.arr = [self.prev]
+            elif self.count == self.max_count:
+                self.arr.append(self.prev)
             
-            dfs(node.right)
 
-        max_streak = 0
-        curr_streak = 0
-        curr_num = 0
-        ans = []
-        dfs(root)
-        return ans
+
+            helper(root.right)
+        self.prev = 0
+        helper(root)
+        return self.arr
