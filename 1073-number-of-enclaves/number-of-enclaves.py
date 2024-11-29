22ea8d12abce796af2d737553c8bd5d51bd9aab5
malhar07 <43933328+malhar07@users.noreply.github.com>
2024-11-29 16:19:22 -0500

    Time: 111 ms (43.04%) | Memory: 24.9 MB (71.10%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.py
+++ 1073-number-of-enclaves/number-of-enclaves.py
@@ -1,19 +1,55 @@
+# class Solution:
+#     def numEnclaves(self, grid: List[List[int]]) -> int:
+#         m, n = len(grid), len(grid[0])
+#         directions = [[0,1], [0,-1], [1,0], [-1,0]]
+        
+        
+#         def bfs(row, col):
+#             q = deque()
+#             q.append((row,col))
+#             path = set()
+#             path.add((row,col))
+
+#             while q:
+#                 for i in range(len(q)):
+#                     row, col = q.popleft()
+#                     grid[row][col] = 0
+#                     for dr, dc in directions:
+#                         newr, newc = row+dr, col+dc
+
+#                         if 0<=newr<m and 0<= newc < n and grid[newr][newc] == 1 and (newr, newc) not in path:
+#                             path.add((newr, newc))
+#                             q.append((newr, newc))
+        
+#         for row in range(m):
+#             for col in range(n):
+#                 if row in [0, m-1] or col in [0, n-1]:
+                    
+#                     if grid[row][col] == 1:
+#                         print(row, " ", col)
+#                         bfs(row, col)
+#         res = 0
+#         for i in grid:
+#             res+=sum(i)
+#         return res
+
 class Solution:
     def numEnclaves(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
         directions = [[0,1], [0,-1], [1,0], [-1,0]]
+        path = set()
         
         
         def bfs(row, col):
             q = deque()
             q.append((row,col))
-            path = set()
+            
             path.add((row,col))
 
             while q:
                 for i in range(len(q)):
                     row, col = q.popleft()
-                    grid[row][col] = 0
+                    # grid[row][col] = 0
                     for dr, dc in directions:
                         newr, newc = row+dr, col+dc
 
@@ -31,4 +67,4 @@ class Solution:
         res = 0
         for i in grid:
             res+=sum(i)
-        return res
+        return res - len(path)
