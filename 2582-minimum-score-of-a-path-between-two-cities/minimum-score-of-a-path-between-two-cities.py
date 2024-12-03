bc7d31ea80d5a3a0d183e132ae90f9f00942cf2b
malhar07 <43933328+malhar07@users.noreply.github.com>
2024-12-03 02:23:41 -0500

    Time: 272 ms (56.34%) | Memory: 77.7 MB (24.44%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.py
@@ -2,10 +2,11 @@ class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
         adjlist = {i:[] for i in range(1,n+1)}
         for a,b,dist in roads:
-            adjlist[a].append(b)
-            adjlist[b].append(a)
+            adjlist[a].append((b,dist))
+            adjlist[b].append((a,dist))
         visited = set()
         
+        self.dist = 10001
         def bfs():
             q = deque()
             q.append((1))
@@ -13,15 +14,17 @@ class Solution:
             visited.add(1)
             while q:
                 curr = q.popleft()
-                for nei in adjlist[curr]:
+                for nei, d in adjlist[curr]:
+                    self.dist = min(self.dist, d)
                     if nei not in visited:
                         q.append(nei)
                         visited.add(nei)
+            return self.dist
 
-        bfs()
-        dist = 10001
+        return bfs()
+        # dist = 10001
 
-        for road in roads:
-            if road[0] in visited:
-                dist = min(dist, road[2])
-        return dist
+        # for road in roads:
+        #     if road[0] in visited:
+        #         dist = min(dist, road[2])
+        # return dist
