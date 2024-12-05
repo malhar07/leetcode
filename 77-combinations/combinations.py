62eaf209ed556a81687a8243adc37d94e931d722
malhar07 <43933328+malhar07@users.noreply.github.com>
2024-12-05 14:51:28 -0500

    Time: 194 ms (19.41%) | Memory: 59.1 MB (41.88%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,20 +1,16 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        subset = []
         res = []
+        def helper(num, subset):
 
-        def helper(num):
             if len(subset) == k:
-                res.append(subset[:])
-                return 
-
-            if num > n:
+                res.append(subset)
                 return
 
+            if num > n :
+                return 
+            helper(num+1, subset[:])
             subset.append(num)
-            helper(num+1)
-
-            subset.pop()
-            helper(num+1)
-        helper(1)
+            helper(num+1, subset[:])
+        helper(1, [])
         return res
