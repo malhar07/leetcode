2d5f999562a59c135428be82912206e03b565bf9
malhar07 <43933328+malhar07@users.noreply.github.com>
2024-12-06 17:54:14 -0500

    Time: 4 ms (13.78%) | Memory: 17.7 MB (5.17%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -4,14 +4,15 @@ class Solution:
 
         def helper(res, nums):
             if len(nums) == 0:
-                ans.append(res)
-                return
+                # ans.append(res)
+                return [res]
+            curr = []
             
             for i in range(len(res)+1):
 
                 # temp = res[:i] + [nums[0]] + res[i:]
+                curr.extend(helper(res[:i] + [nums[0]] + res[i:], nums[1:]))
+            return curr
 
-                helper(res[:i] + [nums[0]] + res[i:], nums[1:])
-
-        helper([], nums)
-        return ans
+        
+        return helper([], nums)
