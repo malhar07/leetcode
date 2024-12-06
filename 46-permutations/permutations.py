3cea4561b8b8c4d9a281a73a16882ee0dbc9ef42
malhar07 <43933328+malhar07@users.noreply.github.com>
2024-12-06 18:22:08 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (5.17%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,15 +1,17 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        res = []
+        # res = []
         def helper(p, up):
             #base case
             if len(up) == 0:
-                res.append(p)
-                return 
+                # res.append(p)
+                return [p]
+            curr = []
             for i in range(len(p)+1):
                 perm = p[0:i] + [up[0]] + p[i:]
-                helper(perm, up[1:])
-        helper([], nums)
-        return res
+                curr.extend(helper(perm, up[1:]))
+            return curr
+        
+        return helper([], nums)
 
 
