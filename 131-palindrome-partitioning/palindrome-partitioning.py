38f0294b34f56a2aa16b06fb2db9a38efdfd39aa
malhar07 <43933328+malhar07@users.noreply.github.com>
2024-12-13 15:11:56 -0500

    Time: 44 ms (75.79%) | Memory: 33.4 MB (75.76%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -1,28 +1,39 @@
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
+        dict1 = {}
         def isPal(s):
             left = 0
             right = len(s)-1
 
             while left < right:
                 if s[left] != s[right]:
+                    dict1[s] = False
                     return False
                 left+=1
                 right-=1
+            dict1[s] = True
             return True
         
         def helper(ind, pal):
             if ind == len(s):
                 res.append(pal[:])
                 return
-            # if ind>= len(s):
-            #     return
 
             for i in range(len(s[ind:])):
-                if isPal(s[ind:ind+i+1]):
-                    pal.append(s[ind:ind+i+1])
-                    helper(ind+i+1, pal)
-                    pal.pop()
+                curr = s[ind:ind+i+1]
+                if curr in dict1:
+                    if dict1[curr]:
+                        pal.append(s[ind:ind+i+1])
+                        helper(ind+i+1, pal)
+                        pal.pop()
+                else:
+                    if isPal(curr):
+                        pal.append(curr)
+                        helper(ind+i+1, pal)
+                        pal.pop()
+
+                
+                    
         helper(0, [])
         return res
