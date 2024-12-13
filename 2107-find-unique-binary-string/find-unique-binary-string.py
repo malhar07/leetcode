6bcfbc1f4cbc1f5bb857ff2c25ae0e1700074d81
malhar07 <43933328+malhar07@users.noreply.github.com>
2024-12-13 18:46:17 -0500

    Time: 0 ms (100.00%) | Memory: 17.4 MB (21.33%) - LeetSync

--- 2107-find-unique-binary-string/find-unique-binary-string.py
+++ 2107-find-unique-binary-string/find-unique-binary-string.py
@@ -5,15 +5,16 @@ class Solution:
 
         def helper(s):
             nonlocal ans
-            # prin,t(s)
             if len(s) == length:
                 if s not in nums:
-                    # print(s)
-                    ans = s[:]
+                    return s[:]
+                    # ans = s[:]
                 return
-                
-            helper(s+"0")
-            # s=s[:-1]
-            helper(s+"1")
+            
+            add_zero = helper(s+"0")
+            if add_zero:
+                return add_zero
+            
+            return helper(s+"1")
         helper("")
-        return ans
+        return helper("")
