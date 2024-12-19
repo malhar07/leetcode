70a5302dae3dd53ad33300b70a94de266bb56dbf
malhar07 <43933328+malhar07@users.noreply.github.com>
2024-12-19 15:01:58 -0500

    Time: 172 ms (45.24%) | Memory: 41.5 MB (12.74%) - LeetSync

--- 883-car-fleet/car-fleet.py
+++ 883-car-fleet/car-fleet.py
@@ -1,17 +1,15 @@
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         arr = []
+
         for ind, i in enumerate(position):
             arr.append((i, (target-i)/speed[ind]))
-        
+
         arr = sorted(arr,key = lambda x:x[0])
-        print(arr)
         fleets = 1
         curr = arr[-1][1]
         for i in range(len(arr)-1, -1, -1):
-            if arr[i][1] <= curr:
-                continue
-            else:
+            if arr[i][1] > curr:
                 curr = arr[i][1]
                 fleets+=1
         return fleets
