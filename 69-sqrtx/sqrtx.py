79455ef8b7a3bc9e0b2d96e327fc4bc9c5fdd918
malhar07 <43933328+malhar07@users.noreply.github.com>
2024-12-19 19:48:20 -0500

    Time: 2 ms (67.45%) | Memory: 17.7 MB (6.99%) - LeetSync

---
+++ 69-sqrtx/sqrtx.py
@@ -0,0 +1,18 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        left = 0
+        right = x
+        res = 0
+
+        while left <= right:
+            mid = (left + right)//2
+
+            if mid**2 < x:
+                res = mid
+                left = mid+1
+            elif mid**2 == x:
+                return mid
+            else:
+                right = mid-1
+        return res
+            
