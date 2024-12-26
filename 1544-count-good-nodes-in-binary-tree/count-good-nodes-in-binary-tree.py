5acdfd75fb8cb5b3f9404388f54a56ce87c01e58
malhar07 <43933328+malhar07@users.noreply.github.com>
2024-12-26 14:36:27 -0500

    Time: 225 ms (5.10%) | Memory: 32.3 MB (9.41%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -6,18 +6,32 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
+        # res = 0
+        # def helper(root, curr):
+        #     nonlocal res
+        #     if root == None:
+        #         return 0
+        #     if root.val >= curr:
+        #         res += 1
+            
+        #     helper(root.left, max(root.val, curr))
+        #     helper(root.right, max(root.val,curr))
+        # helper(root, -10001)
+        # return res
         res = 0
         def helper(root, curr):
-            nonlocal res
             if root == None:
                 return 0
+            res = 0
+            
+            
+            left = helper(root.left, max(root.val, curr))
+            right = helper(root.right, max(root.val,curr))
             if root.val >= curr:
                 res += 1
-            
-            helper(root.left, max(root.val, curr))
-            helper(root.right, max(root.val,curr))
+            return left+right+res
         helper(root, -10001)
-        return res
+        return helper(root, -10001)
 
             
 
