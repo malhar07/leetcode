4aacf8b4711c36609a5de211cb860077bbb46a1e
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-01-03 15:54:21 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (5.52%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -12,6 +12,25 @@ class Solution:
             return False
         else:
             return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        # stack1 = [p]
+        # stack2 = [q]
+
+        # if not p and not q:
+        #     return True
+        
+        # while stack1 and stack2:
+        #     root1 = stack1.pop()
+        #     root2 = stack2.pop()
+        #     if not self.check(root1, root2):
+        #         return False
+        #     else:
+        #         if root1:
+        #             stack1.append(root1.left)
+        #             stack1.append(root1.right)
+
+        #             stack2.append(root2.left)
+        #             stack2.append(root2.right)
+        # return True
     
     def check(self, root1, root2):
         if not root1 and not root2:
@@ -20,4 +39,5 @@ class Solution:
             return False
         if root1.val == root2.val:
             return True
-        # return False
+        return False
+        
