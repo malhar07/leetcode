7f6e213a88ac0aa074e42ad07e37134d547fa3ee
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-01-10 15:04:58 -0500

    Time: 0 ms (100.00%) | Memory: 20 MB (38.03%) - LeetSync

--- 419-battleships-in-a-board/battleships-in-a-board.py
+++ 419-battleships-in-a-board/battleships-in-a-board.py
@@ -3,22 +3,22 @@ class Solution:
         count = 0
         row = len(board)
         col = len(board[0])
-        visited = set()
-        directions = [[0,1], [1,0], [-1,0], [0,-1]]
-        def bfs(r,c):
-            q = deque()
-            q.append((r,c))
-            visited.add((r,c))
+        # visited = set()
+        # directions = [[0,1], [1,0], [-1,0], [0,-1]]
+        # def bfs(r,c):
+        #     q = deque()
+        #     q.append((r,c))
+        #     visited.add((r,c))
 
-            while q:
-                for i in range(len(q)):
-                    r,c = q.popleft()
-                    for dr, dc in directions:
-                        newr = r+dr
-                        newc = c+dc
-                        if 0<=newr<row and 0<=newc<col and board[newr][newc] == "X" and (newr,newc) not in visited:
-                            visited.add((newr,newc))
-                            q.append((newr,newc))
+        #     while q:
+        #         for i in range(len(q)):
+        #             r,c = q.popleft()
+        #             for dr, dc in directions:
+        #                 newr = r+dr
+        #                 newc = c+dc
+        #                 if 0<=newr<row and 0<=newc<col and board[newr][newc] == "X" and (newr,newc) not in visited:
+        #                     visited.add((newr,newc))
+        #                     q.append((newr,newc))
         def check(r,c):
             if (r-1 < 0 or board[r-1][c] == ".") and (c-1<0 or board[r][c-1]=="."):
                 return True
@@ -28,8 +28,8 @@ class Solution:
         
         for i in range(row):
             for j in range(col):
-                print(i, " ", j)
-                if board[i][j] == 'X' and (i,j) not in visited:
+                # print(i, " ", j)
+                if board[i][j] == 'X':# and (i,j) not in visited:
                     # bfs(i,j)
                     count+=check(i,j)
                     # count+=1
