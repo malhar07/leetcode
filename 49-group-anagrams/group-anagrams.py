10650acb309bf719756b4200d3ddccaa12972ed5
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-01-09 22:53:11 -0500

    Time: 27 ms (12.51%) | Memory: 20.9 MB (46.11%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,23 +1,5 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # ans = []
-        # dict1 = {}
-
-        # for word in strs:
-        #     w = str(sorted(word))
-        #     if w in dict1:
-        #         lst = dict1[w]
-        #         lst.append(word)
-        #         dict1[w] = lst #dict1[tuple(arr)].append(word)
-        #     else:
-        #         dict1[w] = [word]
-        # print(dict1)
-        # # for i in dict1.values():
-        # #     # print(i)
-        # #     ans.append(i)
-        # res = dict1.values()
-        # return res
-
         dict1 = {}
         res = []
         for word in strs:
