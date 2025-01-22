ad850c9e19525eda14cb3d291c83a6ab33c9917b
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-01-22 18:18:22 -0500

    Time: 41 ms (53.74%) | Memory: 35.4 MB (47.30%) - LeetSync

---
+++ 3094-minimum-number-of-operations-to-make-array-empty/minimum-number-of-operations-to-make-array-empty.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minOperations(self, nums: List[int]) -> int:
+        dict1 = {}
+        res = 0
+        for i in nums:
+            dict1[i] = dict1.get(i, 0)+1
+        for val in dict1.values():
+            while val>0:
+                if val == 1:
+                    return -1
+                if val%3 == 0:
+                    res += val//3
+                    val = 0
+                else:
+                    val-=2
+                    res+=1
+        return res
+            
