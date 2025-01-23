5e07e4c2add9b1519ca148961f7fc9f2084ed4b5
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-01-22 20:38:54 -0500

    Time: 28 ms (29.83%) | Memory: 26.2 MB (79.99%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,8 +1,8 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         dict1 = {}
-        if k == 0:
-            return False
+        # if k == 0:
+        #     return False
         
         for i in range(len(nums)):
             if nums[i] in dict1:
@@ -11,10 +11,4 @@ class Solution:
                 dict1[nums[i]] = 1
             if len(dict1)>k:
                 del dict1[nums[i-k]]
-        # for i in range(k, len(nums)):
-        #     if nums[i] in dict1:
-        #         return True
-        #     else:
-        #         del dict1[nums[i-k]]
-        #         dict1[nums[i]] = 1
         return False
