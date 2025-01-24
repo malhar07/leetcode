6de109df6ce519c4be72305593d3efd698b87467
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-01-23 19:58:13 -0500

    Time: 207 ms (23.24%) | Memory: 24.2 MB (11.17%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,33 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        # [1,2,1,2,3,4,2,2,1,3]
+        dict1 = {}
+        count = 0
+        left = 0
+        res = 0
+
+        for i in range(len(fruits)):
+            if fruits[i] in dict1:
+                dict1[fruits[i]] += 1
+            else:
+                dict1[fruits[i]] = 1
+                count+=1
+                if count > 2:
+                    curr = fruits[left]
+                    print(curr)
+                    while count > 2:
+                        dict1[fruits[left]] -= 1
+                        if dict1[fruits[left]] == 0:
+                            del dict1[fruits[left]]
+                            count-=1
+                        left += 1
+
+                    # while dict1[curr] > 0:
+                    #     # print(left, " ", dict1[curr])
+                    #     # if fruits[left] == curr:
+                        
+                    # del dict1[curr]
+                    # count-=1
+            res = max(res, i-left+1)
+        return res
+
