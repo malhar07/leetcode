c330505f8df36cf6624f8358cf90ef9c82e92aef
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-01-27 17:19:53 -0500

    Time: 27 ms (72.47%) | Memory: 18.8 MB (15.91%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
@@ -1,44 +1,16 @@
 class Solution:
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        # arr = []
-        # for i in range(len(s)):
-        #     arr.append(abs(ord(s[i]) - ord(t[i])))
-
-        # cost = 0
-        # left = 0
-        # right = 0
-        # max_len = 0
-        # while right < len(s):
-        #     if cost + arr[right] <= maxCost:
-        #         cost += arr[right]
-        #         right += 1
-        #     else:
-        #         cost += arr[right] - arr[left]
-        #         right += 1
-        #         left +=1
-        #     if cost <= maxCost:
-        #         max_len = max(max_len, right - left)
-        # return max_len
-
-        # arr = []
-        # for i in range(len(s)):
-        #     arr.append(abs(ord(s[i]) - ord(t[i])))
-
-        cost = 0
+        arr = []
+        for i in range(len(s)):
+            arr.append(abs(ord(s[i]) - ord(t[i])))
         left = 0
-        right = 0
-        max_len = 0
-        while right < len(s):
-            x = abs(ord(s[right]) - ord(t[right]))
-            y = abs(ord(s[left]) - ord(t[left]))
-            if cost + x <= maxCost:
-                cost += x
-                right += 1
-            else:
-                cost += x - y
-                right += 1
-                left +=1
-            if cost <= maxCost:
-                max_len = max(max_len, right - left)
-        return max_len
-        
+        cost = 0
+        res = 0
+        for right in range((len(arr))):
+            cost += arr[right]
+
+            while cost>maxCost:
+                cost-=arr[left]
+                left+=1
+            res = max(res, right-left+1)
+        return res
