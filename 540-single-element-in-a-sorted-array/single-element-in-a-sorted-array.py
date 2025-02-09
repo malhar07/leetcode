75e3db43f5846831ca437868507d6ea037445132
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-02-08 22:23:48 -0500

    Time: 0 ms (100.00%) | Memory: 24.8 MB (41.94%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.py
@@ -1,27 +1,24 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
+        # even and last then return
         left = 0
         right = len(nums)-1
-
-        if len(nums) == 1:
-            return nums[0]
-
-
+        res = 0
         while left <= right:
             mid = (left+right)//2
-            if mid == len(nums)-1 or (nums[mid-1] != nums[mid] and nums[mid+1] != nums[mid]):
-                return nums[mid]
-            else:
-                if (len(nums) - mid - 1) % 2 == 0:
-                    if nums[mid+1] != nums[mid]:
-                        right = mid-1
-                    else:
-                        left = mid+1
 
+            if mid%2 == 0:
+                if mid == right:
+                    return nums[mid]
+                if nums[mid] != nums[mid+1]:
+                    res = mid
+                    right = mid-1
                 else:
-                    if nums[mid+1] != nums[mid]:
-                        left = mid+1
-                    else:
-                        right = mid-1
-
-
+                    left = mid+1
+            else:
+                if nums[mid] != nums[mid-1]:
+                    res = mid
+                    right = mid-1
+                else:
+                    left = mid+1
+        return nums[res]
