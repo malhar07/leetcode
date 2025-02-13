16b32f60b54bd820eb84be59e3cde7e32fc1dae6
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-02-13 17:34:40 -0500

    Time: 0 ms (100.00%) | Memory: 18.1 MB (18.09%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -7,22 +7,9 @@ class Solution:
             mid = (left+right)//2
             if mid-1 < 0 or nums[mid-1] < nums[mid]:
                 if mid+1 == len(nums) or nums[mid+1] <nums[mid]:
-                    print("vvvvvvvv")
                     return mid
                 else:
                     left = mid+1
             else:
                 right = mid-1
         return mid
-        #     print(mid)
-        #     if nums[mid-1]<nums[mid]<nums[mid+1]:
-        #         left = mid+1
-        #     elif nums[mid-1]>nums[mid]>nums[mid+1]:
-        #         right = mid-1
-        #     elif nums[mid-1]>nums[mid] and nums[mid+1]>nums[mid]:
-        #         left = mid+1
-        #     else:
-        #         return mid
-
-        # return mid
-            
