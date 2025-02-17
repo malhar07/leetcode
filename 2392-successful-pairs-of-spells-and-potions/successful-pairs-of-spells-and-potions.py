bcf8822fe79cd3330296357b022e845a405b7063
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-02-16 20:28:03 -0500

    Time: 522 ms (51.09%) | Memory: 38.7 MB (80.63%) - LeetSync

--- 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.py
@@ -1,13 +1,14 @@
 class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
         res = []
+        n = len(potions)
         potions.sort()
 
         for i in spells:
             ind = -1
             count = 0
             left = 0
-            right = len(potions)-1
+            right = n-1
             while left<=right:
                 mid = (left+right)//2
                 if potions[mid]*i >= success:
@@ -18,5 +19,5 @@ class Solution:
             if ind == -1:
                 res.append(0)
             else:
-                res.append(len(potions)-ind)
+                res.append(n-ind)
         return res
