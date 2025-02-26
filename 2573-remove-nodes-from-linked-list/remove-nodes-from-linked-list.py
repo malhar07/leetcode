8e9e988ed38427778485d33f569a68ed4a8a29ce
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-02-26 05:41:19 -0500

    Time: 469 ms (5.01%) | Memory: 59 MB (25.79%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.py
@@ -5,31 +5,50 @@
 #         self.next = next
 class Solution:
     def removeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        # 13,10,7,5,4,2,8
+        
+        20,5,2,13,3,8
+        # start = ListNode()
+        # start.next = head
+        # ptr = start
+        # temp = start
+        # _min = 100001
+
+        # while ptr.next:
+        #     _min = min(_min, ptr.next.val)
+        #     print(_min)
+        #     if ptr.next.val > _min:
+        #         while temp.next.val> ptr.next.val:
+        #             temp = temp.next
+        #         temp.next = ptr.next
+        #         ptr = temp.next
+        #         temp = start
+        #     else:
+        #         ptr = ptr.next
+        # return start.next
         arr = []
         temp = head
+        if not head:
+            return None
         while temp:
-            arr.append(temp.val)
+            arr.append([temp.val,False])
             temp = temp.next
-        
-        ind = len(arr)-2
-        max1 = arr[-1]
-        while ind >= 0:
-            temp = arr[ind]
-            arr[ind] = max1
-            if temp > max1:
-                max1 = temp
-            ind-=1
-        temp = head
-        ind = 1
+        curr = arr[-1][0]
+        for i in range(len(arr)-1,-1,-1):
+            if arr[i][0] < curr:
+                arr[i][1] = True
+            else:
+                curr = arr[i][0]
+        print(arr)
+        start = ListNode()
+        temp = start
+        start.next = head
+        ind = 0
         while temp.next:
-            if temp.next.val < arr[ind]:
+            if arr[ind][1] == True:
                 temp.next = temp.next.next
-                ind+=1
             else:
                 temp = temp.next
-                ind+=1
-        print(arr)
-        if head.val < arr[0]:
-            return head.next
-        
-        return head
+            ind+=1
+            
+        return start.next
