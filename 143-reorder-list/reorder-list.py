c3a6f1886ab3d390f48a56119c67dab484971cac
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-02-26 16:54:36 -0500

    Time: 8 ms (10.13%) | Memory: 23.4 MB (12.71%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -8,18 +8,52 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
-        stack = []
-        temp = head
-        length = 0
-        while temp:
-            stack.append(temp)
-            temp = temp.next
-            length+=1
-        temp = head
-        for i in range(length//2):
-            curr = stack.pop()
-            curr.next = temp.next
-            temp.next = curr
-            temp = temp.next.next
-        temp.next = None
+        # stack = []
+        # temp = head
+        # length = 0
+        # while temp:
+        #     stack.append(temp)
+        #     temp = temp.next
+        #     length+=1
+        # temp = head
+        # for i in range(length//2):
+        #     curr = stack.pop()
+        #     curr.next = temp.next
+        #     temp.next = curr
+        #     temp = temp.next.next
+        # temp.next = None
+        # return head
+        slow = head
+        fast = head
+
+        while fast.next and fast.next.next:
+            slow = slow.next
+            fast = fast.next.next
+        fast = slow.next
+        slow.next = None
+        # def reverse(head):
+        prev = None
+        curr = fast
+        # curr = head
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        # return prev
+        # def merge(l1, l2):
+        #     head = l1
+        l2 = prev
+        l1 = head
+        while l2:
+            curr = l2
+            l2 = l2.next
+            curr.next = l1.next
+            l1.next = curr
+
+            l1 = l1.next.next
         return head
+            # return head
+        # l2 = reverse(fast)
+        # print(l2)
+        # return merge(head, l2)
