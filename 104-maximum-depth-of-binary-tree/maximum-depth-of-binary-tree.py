8b8377dc1c933141cade3959865dc66bab2e4e39
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-01 18:00:59 -0500

    Time: 3 ms (30.69%) | Memory: 19.3 MB (7.62%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -6,11 +6,19 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        def helper(root, depth):
-            if not root:
-                return 0 
-            left = helper(root.left, depth+1)
-            right = helper(root.right, depth+1)
+        # def helper(root, depth):
+        #     if not root:
+        #         return 0 
+        #     left = helper(root.left, depth+1)
+        #     right = helper(root.right, depth+1)
 
-            return max(left,right,depth)
-        return helper(root,1)
+        #     return max(left,right,depth)
+        # return helper(root,1)
+
+        def helper(root):
+            if not root:
+                return 0
+            left = helper(root.left)
+            right = helper(root.right)
+            return max(left,right)+1
+        return helper(root)
