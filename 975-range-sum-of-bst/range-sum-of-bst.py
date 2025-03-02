4124215ce1932fa1485eb146577714a1e307cbd1
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-02 01:47:34 -0500

    Time: 4 ms (49.55%) | Memory: 25.2 MB (58.49%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.py
+++ 975-range-sum-of-bst/range-sum-of-bst.py
@@ -6,33 +6,41 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
-        # def helper(root):
-        #     if not root:
-        #         return 0
-        #     left = helper(root.left)
-        #     right = helper(root.right)
-        #     if low<=root.val<=high:
-        #         return left+right+root.val
-        #     return left+right
-        # return helper(root)
-
-        res = 0
-        if not root:
-            return res
-        stack = [root]
-        while stack:
-            curr = stack.pop()
-            if curr.val < low:
-                if curr.right:
-                    stack.append(curr.right)
-            elif curr.val>high:
-                if curr.left:
-                    stack.append(curr.left)
+        def helper(root):
+            if not root:
+                return 0
+            if root.val > low:
+                left = helper(root.left)
             else:
-                print(curr.val)
-                res+=curr.val
-                if curr.left:
-                    stack.append(curr.left)
-                if curr.right:
-                    stack.append(curr.right)
-        return res
+                left = 0
+            if root.val < high:
+                right = helper(root.right)
+            else:
+                right = 0
+
+            # right = helper(root.right)
+            if low<=root.val<=high:
+                return left+right+root.val
+            return left+right
+        return helper(root)
+
+        # res = 0
+        # if not root:
+        #     return res
+        # stack = [root]
+        # while stack:
+        #     curr = stack.pop()
+        #     if curr.val < low:
+        #         if curr.right:
+        #             stack.append(curr.right)
+        #     elif curr.val>high:
+        #         if curr.left:
+        #             stack.append(curr.left)
+        #     else:
+        #         print(curr.val)
+        #         res+=curr.val
+        #         if curr.left:
+        #             stack.append(curr.left)
+        #         if curr.right:
+        #             stack.append(curr.right)
+        # return res
