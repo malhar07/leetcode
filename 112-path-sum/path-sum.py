af19c3ab65e53dc0a85db32e31c215fdc8a556ea
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-02 01:31:49 -0500

    Time: 0 ms (100.00%) | Memory: 19.1 MB (23.72%) - LeetSync

--- 112-path-sum/path-sum.py
+++ 112-path-sum/path-sum.py
@@ -6,17 +6,28 @@
 #         self.right = right
 class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
-        def helper(root, _sum):
-            if not root:
-                return False
-            if _sum+root.val == targetSum and not root.left and not root.right:
-                return True
-            # return False
+        # def helper(root, _sum):
+        #     if not root:
+        #         return False
+        #     if _sum+root.val == targetSum and not root.left and not root.right:
+        #         return True
 
-            left = helper(root.left, _sum+root.val)
-            right = helper(root.right, _sum+root.val)
-            return left or right
+        #     left = helper(root.left, _sum+root.val)
+        #     right = helper(root.right, _sum+root.val)
+        #     return left or right
 
+        # if not root:
+        #     return False
+        # return helper(root,0)
         if not root:
             return False
-        return helper(root,0)
+        stack = [(root,0)]
+        while stack:
+            curr, _sum = stack.pop()
+            if curr.val+_sum == targetSum and not curr.left and not curr.right:
+                return True
+            if curr.left:
+                stack.append((curr.left,curr.val+_sum))
+            if curr.right:
+                stack.append((curr.right,curr.val+_sum))
+        return False
