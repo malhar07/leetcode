5860819b6e2ce533551aa5546969e9eafce670e9
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-02 01:12:22 -0500

    Time: 3 ms (49.63%) | Memory: 18.1 MB (68.72%) - LeetSync

--- 617-merge-two-binary-trees/merge-two-binary-trees.py
+++ 617-merge-two-binary-trees/merge-two-binary-trees.py
@@ -6,21 +6,51 @@
 #         self.right = right
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        # if not root1 and not root2:
+        #     return None
+        
+        # def helper(p,q):
+        #     if not p and not q:
+        #         return None
+        #     if not p:
+        #         return q
+        #     if not q:
+        #         return p
+        #     p.left = helper(p.left, q.left)
+        #     p.right = helper(p.right, q.right)
+        #     p.val = p.val+q.val
+        #     return p
+        # return helper(root1, root2)
+
         if not root1 and not root2:
             return None
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+        root1.val += root2.val
         
-        def helper(p,q):
-            if not p and not q:
-                return None
-            if not p:
-                return q
-            if not q:
-                return p
-            p.left = helper(p.left, q.left)
-            p.right = helper(p.right, q.right)
-            p.val = p.val+q.val
-            return p
-        return helper(root1, root2)
+        stack = [(root1, root2)]
+
+        while stack:
+            p,q = stack.pop()
+
+            if not p.left and q.left:
+                p.left = q.left
+            elif p.left and q.left:
+                p.left.val+=q.left.val
+                stack.append((p.left,q.left))
+
+            if not p.right and q.right:
+                p.right = q.right
+            elif p.right and q.right:
+                p.right.val += q.right.val
+                stack.append((p.right,q.right))
+        return root1
+
+            
+
+
 
 
 
