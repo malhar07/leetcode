20fe680cc8f0d6edc67770f31307a6d559b9c9fb
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-02 21:24:21 -0500

    Time: 2 ms (80.03%) | Memory: 20 MB (5.99%) - LeetSync

--- 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
+++ 662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.py
@@ -6,20 +6,29 @@
 #         self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+
+
+
+
+
         level = []
+        self.res = 1
 
         def helper(root, rank, depth):
             if not root:
                 return
             if depth == len(level):
                 level.append([])
+            if len(level[depth]) > 0:
+                self.res = max(self.res, rank-level[depth][0]+1)
             level[depth].append(rank)
             helper(root.left, rank*2, depth+1)
             helper(root.right, rank*2+1, depth+1)
         helper(root, 1, 0)
-        res = 1
-        print(level)
-        for l in level:
-            if len(level) >= 2:
-                res = max(res, l[-1]-l[0]+1)
-        return res
+        return self.res
+        # res = 1
+        # print(level)
+        # for l in level:
+        #     if len(level) >= 2:
+        #         res = max(res, l[-1]-l[0]+1)
+        # return res
