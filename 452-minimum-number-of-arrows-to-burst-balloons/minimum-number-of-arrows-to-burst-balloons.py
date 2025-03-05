7f642ba1713a2cedf343e6e6f9ddd22f67f8d39e
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-04 20:01:00 -0500

    Time: 203 ms (12.77%) | Memory: 51.4 MB (34.64%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.py
@@ -6,7 +6,7 @@ class Solution:
         res.append(points[0])
         for i in range(1,len(points)):
             if points[i][0] <= res[-1][1]:
-                res[-1][0] = min(res[-1][0], points[i][0])
+                res[-1][0] = max(res[-1][0], points[i][0])
                 res[-1][1] = min(res[-1][1], points[i][1])
             else:
                 res.append(points[i])
