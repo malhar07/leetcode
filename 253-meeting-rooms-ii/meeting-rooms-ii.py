eeb7776bd1bd0620bea24c5e74685649a7d26196
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-04 19:34:12 -0500

    Time: 5 ms (42.14%) | Memory: 20.2 MB (26.51%) - LeetSync

--- 253-meeting-rooms-ii/meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/meeting-rooms-ii.py
@@ -1,17 +1,33 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        intervals.sort()
-        res = 1
+        # intervals.sort()
+        # res = 1
 
-        heap = []
-        heapq.heappush(heap,intervals[0][1])
+        # heap = []
+        # heapq.heappush(heap,intervals[0][1])
 
-        for i in range(1, len(intervals)):
-            if intervals[i][0] < heap[0]:
-                # heapq.heappush(hep, intervals[i][1])
-                res = max(res, len(heap)+1)
+        # for i in range(1, len(intervals)):
+        #     if intervals[i][0] < heap[0]:
+        #         res = max(res, len(heap)+1)
+        #     else:
+        #         while heap and heap[0] <= intervals[i][0]:
+        #             heapq.heappop(heap)
+        #     heapq.heappush(heap, intervals[i][1])
+        # return res
+
+        # intervals.sort()
+
+        start = sorted([i[0] for i in intervals])
+        end = sorted([i[1] for i in intervals])
+        s,e = 0,0
+        count, res = 0,0
+
+        while s < len(start):
+            if start[s] < end[e]:
+                count+=1
+                s+=1
             else:
-                while heap and heap[0] <= intervals[i][0]:
-                    heapq.heappop(heap)
-            heapq.heappush(heap, intervals[i][1])
+                count-=1
+                e+=1
+            res = max(res,count)
         return res
