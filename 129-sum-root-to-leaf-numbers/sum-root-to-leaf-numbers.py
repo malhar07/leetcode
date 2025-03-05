9c1d6bd0b8f70ce1f663455c31a58987f4db22c9
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-05 03:59:53 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (86.57%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.py
@@ -6,14 +6,18 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        res = []
-        def helper(root, num):
+        self.res = 0
+        def helper(root,s):
             if not root:
                 return
-            num = num*10 + root.val
-            if not root.right and not root.left:
-                res.append(num)
-            helper(root.left, num)
-            helper(root.right, num)
-        helper(root, 0)
-        return sum(res)
+            s+=str(root.val)
+            if not root.left and not root.right:
+                self.res+=int(s)
+                s = s[:-1]
+                return
+            helper(root.left,s)
+            helper(root.right,s)
+        helper(root,"")
+        return self.res
+
+            
