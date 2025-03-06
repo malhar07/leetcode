be99e575ace0378c4eba909d6585468a942c0ff5
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-06 17:51:42 -0500

    Time: 785 ms (5.04%) | Memory: 58.9 MB (41.92%) - LeetSync

--- 1962-single-threaded-cpu/single-threaded-cpu.py
+++ 1962-single-threaded-cpu/single-threaded-cpu.py
@@ -1,22 +1,29 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
+        heap = []
+        res = []
+        curr_time = 0
         for i in range(len(tasks)):
             tasks[i] = tasks[i] + [i]
+        tasks.sort(key = lambda x: x[0])
         print(tasks)
-        tasks = sorted(tasks, key = lambda x: x[0])
-        avail = []
-        current_time = tasks[0][0]
         ind = 0
-        res = []
-
-        while ind < len(tasks) or len(avail)>0:
-            while ind < len(tasks) and tasks[ind][0] <= current_time:
-                heapq.heappush(avail, [tasks[ind][1], tasks[ind][2]])
+        curr_time = tasks[0][0]
+        while heap or ind < len(tasks):
+            print(curr_time)
+            
+            while ind < len(tasks) and tasks[ind][0] <= curr_time:
+                heapq.heappush(heap, [tasks[ind][1], tasks[ind][2]])
                 ind+=1
-            if len(avail)>0:
-                curr = heapq.heappop(avail)
-                res.append(curr[1])
-                current_time += curr[0]
-            else:
-                current_time = tasks[ind][0]
+            proc_time, index = heapq.heappop(heap)
+            print(proc_time, " ", index)
+
+            res.append(index)
+            curr_time += proc_time
+            if not heap and ind<len(tasks):
+                curr_time = max(curr_time, tasks[ind][0])
+        
         return res
+
+
+
