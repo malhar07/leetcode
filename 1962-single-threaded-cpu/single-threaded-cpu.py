70e8a487e5eb5ee326680d714247e0e8e12a1755
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-06 18:04:58 -0500

    Time: 491 ms (14.40%) | Memory: 58.6 MB (44.14%) - LeetSync

--- 1962-single-threaded-cpu/single-threaded-cpu.py
+++ 1962-single-threaded-cpu/single-threaded-cpu.py
@@ -6,7 +6,7 @@ class Solution:
         for i in range(len(tasks)):
             tasks[i] = tasks[i] + [i]
         tasks.sort(key = lambda x: x[0])
-        print(tasks)
+
         ind = 0
         curr_time = tasks[0][0]
         while heap or ind < len(tasks):
@@ -15,13 +15,15 @@ class Solution:
             while ind < len(tasks) and tasks[ind][0] <= curr_time:
                 heapq.heappush(heap, [tasks[ind][1], tasks[ind][2]])
                 ind+=1
-            proc_time, index = heapq.heappop(heap)
-            print(proc_time, " ", index)
-
-            res.append(index)
-            curr_time += proc_time
-            if not heap and ind<len(tasks):
-                curr_time = max(curr_time, tasks[ind][0])
+            if not heap:
+                curr_time = tasks[ind][0]
+            else:
+                proc_time, index = heapq.heappop(heap)
+                # print(proc_time, " ", index)
+                res.append(index)
+                curr_time += proc_time
+            # if not heap and ind<len(tasks):
+            #     curr_time = max(curr_time, tasks[ind][0])
         
         return res
 
