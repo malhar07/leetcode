56d074de4f097c694aef7ea909952baa652c38db
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-06 19:24:54 -0500

    Time: 3 ms (66.42%) | Memory: 18.2 MB (53.19%) - LeetSync

--- 1184-car-pooling/car-pooling.py
+++ 1184-car-pooling/car-pooling.py
@@ -1,28 +1,17 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        # arr = []
-        # trips.sort(key = lambda x: x[1])
-
-        # for p, f, t in trips:
-        #     while len(arr)>0 and arr[0][0] <= f:
-        #         tp = heapq.heappop(arr)
-        #         capacity+=tp[1]
-        #     if capacity >= p:
-        #         capacity-=p
-        #         heapq.heappush(arr, [t, p])
-        #     else:
-        #         return False
-        # return True
         trips.sort(key = lambda x: x[1])
-        arr = []
-        for i in range(len(trips)):
-            current_time = trips[i][1]
-            while len(arr)>0 and arr[0][0] <= current_time:
-                t, np = heapq.heappop(arr)
-                capacity += np
-            if trips[i][0] <= capacity:
-                capacity-=trips[i][0]
-                heapq.heappush(arr, [trips[i][2], trips[i][0]])
-            else:
+        heap = []
+        pos = 0
+        for num, frm, to in trips:
+            while heap and heap[0][0] <= frm:
+                curr = heapq.heappop(heap)
+                capacity += curr[1]
+            if num> capacity:
                 return False
+            else:
+                capacity-=num
+            heapq.heappush(heap,[to,num])
         return True
+
+             
