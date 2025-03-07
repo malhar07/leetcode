6465627cc15c3ce51afac3486ecc6e0d7bd92f42
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-07 17:42:42 -0500

    Time: 0 ms (100.00%) | Memory: 17.8 MB (90.56%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -1,19 +1,35 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+
         res = []
-        # subset = []
-
-        def helper(ind,subset):
-            if ind>= len(nums):
-                res.append(subset[:])
+        # self.nums = nums
+        def helper(arr, ind):
+            if ind >= len(nums):
+                res.append(arr[:])
                 return
-            
-            helper(ind+1, subset[:])
-
-            subset.append(nums[ind])
-            helper(ind+1, subset[:])
-
-            # subset.pop()
-        helper(0, [])
-
+            arr.append(nums[ind])
+            # ind+=1
+            helper(arr,ind+1)
+            arr.pop()
+            helper(arr,ind+1)
+        helper([],0)
         return res
+            
+        # res = []
+        # # subset = []
+
+        # def helper(ind,subset):
+        #     if ind>= len(nums):
+        #         # res.append(subset[:])
+        #         return [subset[:]]
+            
+        #     left = helper(ind+1, subset[:])
+
+        #     subset.append(nums[ind])
+        #     right = helper(ind+1, subset[:])
+        #     # subset.pop(). Pop is not used beacuse a copy of subset is passed on to the each recursive call
+        #                     # (subset[:]) if a copy of subset is passed then the last element needs to be popped out.
+        #     return left+right
+        # return helper(0, [])
+
+        # # return res
