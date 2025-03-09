9d03d0cf05bd915a3fe41594505e380367861f90
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-08 23:15:26 -0500

    Time: 69 ms (29.37%) | Memory: 25.5 MB (14.73%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.py
+++ 286-walls-and-gates/walls-and-gates.py
@@ -3,29 +3,26 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        row, col = len(rooms), len(rooms[0])
         visited = set()
-        q = deque()
-        dist = 0
+        q= deque([])
+        def bfs(q):
+            directions = [[0,1],[1,0],[0,-1],[-1,0]]
+            dist = 1
+            while q:
+                for i in range(len(q)):
+                    r,c = q.popleft()
+                    for dr,dc in directions:
+                        newr, newc = r+dr, c+dc
 
-        for r in range(row):
-            for c in range(col):
-                if rooms[r][c] == 0:
-                    visited.add((r, c))
-                    q.append((r,c))
+                        if 0<=newr<len(rooms) and 0<=newc<len(rooms[0]) and rooms[newr][newc] != -1 and (newr,newc) not in visited:
+                            rooms[newr][newc] = min(dist, rooms[newr][newc])
+                            q.append((newr,newc))
+                            visited.add((newr,newc))
+                dist+=1
 
-        while q:
-            for i in range(len(q)):
-                r,c = q.popleft()
-                rooms[r][c] = dist
-
-                directions = [[0,1], [0,-1], [1,0], [-1,0]]
-                for dr, dc in directions:
-                    newr = r+dr
-                    newc = c+dc
-                    if newr <0 or newr>=len(rooms) or newc < 0 or newc>=len(rooms[0]) or rooms[newr][newc] == -1 or (newr,newc) in visited:
-                        continue
-                    visited.add((newr,newc))
-                    q.append((newr,newc))
-            dist+=1
-                
+        
+        for i in range(len(rooms)):
+            for j in range(len(rooms[0])):
+                if rooms[i][j] == 0:
+                    q.append((i,j))
+        bfs(q)
