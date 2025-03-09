e9fa23260f0f219ac70de4ad4858e23e36948452
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-08 22:54:09 -0500

    Time: 321 ms (11.02%) | Memory: 25.7 MB (15.00%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -3,21 +3,43 @@ class Solution:
         islands = 0
         visited = set()
 
-        def dfs(x,y):
-            # q = deque([(x,y)])
+        def bfs(x,y):
+            q = deque([(x,y)])
             visited.add((x,y))
-
             directions = [[0,1],[1,0],[-1,0],[0,-1]]
-            for dr, dc in directions:
-                newx,newy = x+ dr, y+dc
-                if 0<=newx<len(grid) and 0<=newy<len(grid[0]) and (newx,newy) not in visited and grid[newx][newy] == "1":       
-                    visited.add((newx, newy))
-                    dfs(newx, newy)
-                
+            while q:
+
+            
+                for i in range(len(q)):
+                    # print("kkbkbkbkbkb")
+                    x,y = q.popleft()
+                    for dr, dc in directions:
+                        newx,newy = x+ dr, y+dc
+                        if 0<=newx<len(grid) and 0<=newy<len(grid[0]) and (newx,newy) not in visited and grid[newx][newy] == "1":
+                            print(newx, " ", newy)
+                            visited.add((newx, newy))
+                            q.append((newx, newy))
+                    
+
+        
 
         for i in range(len(grid)):
             for j in range (len(grid[0])):
                 if grid[i][j] == "1" and (i,j) not in visited:
                     islands += 1
-                    dfs(i,j)
+                    bfs(i,j)
         return islands
+
+
+
+        # def dfs(x,y):
+        #     # q = deque([(x,y)])
+        #     visited.add((x,y))
+
+        #     directions = [[0,1],[1,0],[-1,0],[0,-1]]
+        #     for dr, dc in directions:
+        #         newx,newy = x+ dr, y+dc
+        #         if 0<=newx<len(grid) and 0<=newy<len(grid[0]) and (newx,newy) not in visited and grid[newx][newy] == "1":       
+        #             visited.add((newx, newy))
+        #             dfs(newx, newy)
+                
