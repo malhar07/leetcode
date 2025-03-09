b834561886074cd771afbeceae4696bec8a915fe
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-08 21:26:13 -0500

    Time: 47 ms (6.46%) | Memory: 17.7 MB (83.85%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.py
+++ 40-combination-sum-ii/combination-sum-ii.py
@@ -1,21 +1,27 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
-        res = []
         candidates.sort()
-        def helper(ind, subset, sum1):
-            if sum1 == target:
-                res.append(subset[:])
+        res = []
+
+        def helper(arr,ind):
+            if sum(arr) == target:
+                res.append(arr[:])
                 return
-
-            if ind>=len(candidates) or sum1>target:
-                return 
-
-            subset.append(candidates[ind])
-            helper(ind+1, subset, sum1+candidates[ind])
-
-            subset.pop()
-            while ind+1<len(candidates) and candidates[ind] == candidates[ind+1]:
+            if sum(arr) > target:
+                # res.append(arr[:])
+                return
+            if ind >= len(candidates):
+                return
+            
+            arr.append(candidates[ind])
+            helper(arr,ind+1)
+            arr.pop()
+            while ind<len(candidates)-1 and candidates[ind] == candidates[ind+1]:
                 ind+=1
-            helper(ind+1, subset, sum1)
-        helper(0, [], 0)
+            
+            helper(arr,ind+1)
+            
+
+
+        helper([],0)
         return res
