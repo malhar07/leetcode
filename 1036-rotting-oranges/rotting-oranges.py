148cffc9ca5a83f2b9d14b5b7089e9614e9eb60e
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-08 23:39:16 -0500

    Time: 7 ms (27.48%) | Memory: 17.7 MB (72.31%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -3,6 +3,7 @@ class Solution:
         res = -1
         visited = set()
         q = deque([])
+        self.fresh = 0
 
         def bfs(q):
             directions = [[0,1],[1,0], [0,-1],[-1,0]]
@@ -16,6 +17,7 @@ class Solution:
                         newr, newc = r+dr, c+dc
 
                         if 0<=newr<len(grid) and 0<=newc<len(grid[0]) and grid[newr][newc] == 1 and (newr,newc) not in visited:
+                            self.fresh-=1
                             grid[newr][newc] = 2
                             q.append((newr,newc))
                             visited.add((newr,newc))
@@ -27,13 +29,15 @@ class Solution:
             for j in range(len(grid[0])):
                 if grid[i][j] == 2:
                     q.append((i,j))
-        # if len(q) == 0:
-        #     return 0
+                if grid[i][j] == 1:
+                    self.fresh +=1
 
         res = bfs(q)
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 1:
-                    return -1
+        # for i in range(len(grid)):
+        #     for j in range(len(grid[0])):
+        #         if grid[i][j] == 1:
+        #             return -1
+        if self.fresh>0:
+            return -1
         return max(0, res-1)
 
