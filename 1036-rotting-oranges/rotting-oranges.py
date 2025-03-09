61fadb4c1c12f1dfce23a3888e47e605feb9c5ec
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-03-08 23:37:37 -0500

    Time: 3 ms (82.77%) | Memory: 17.8 MB (72.31%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,39 +1,39 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
+        res = -1
         visited = set()
-        q = deque()
-        self.minutes = 0
+        q = deque([])
 
-        def bfs():
-            directions = [[0,1], [0,-1], [1,0], [-1,0]]
-            
+        def bfs(q):
+            directions = [[0,1],[1,0], [0,-1],[-1,0]]
+            time = 0
 
             while q:
                 for i in range(len(q)):
-                    r,c  = q.popleft()
-
-                    grid[r][c] = 2
+                    r,c = q.popleft()
 
                     for dr, dc in directions:
-                        if 0<= r+dr < len(grid) and 0<= c+dc < len(grid[0]) and grid[r+dr][c+dc] == 1 and (r+dr, c+dc) not in visited:
-                            q.append((r+dr, c+dc))
-                            visited.add((r+dr, c+dc))
-                
-                self.minutes+=1
-            # return minutes
+                        newr, newc = r+dr, c+dc
 
-
-
-
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if grid[row][col] == 2:
-                    q.append((row, col))
-                    visited.add((row,col))
-        bfs()
+                        if 0<=newr<len(grid) and 0<=newc<len(grid[0]) and grid[newr][newc] == 1 and (newr,newc) not in visited:
+                            grid[newr][newc] = 2
+                            q.append((newr,newc))
+                            visited.add((newr,newc))
+                time+=1
+            return time
 
         
-        for row in grid:
-            if 1 in row:
-                return -1
-        return max(0, self.minutes-1)
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 2:
+                    q.append((i,j))
+        # if len(q) == 0:
+        #     return 0
+
+        res = bfs(q)
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    return -1
+        return max(0, res-1)
+
