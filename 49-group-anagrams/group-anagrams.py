cd7fd977fbc0aa270b1c8b36abb0e467e8f5f722
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-05-20 21:23:01 -0700

    Time: 7 ms (98.03%) | Memory: 21.2 MB (47.78%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,22 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        # dict1 = {}
-        # res = []
-        # for word in strs:
-        #     if str(sorted(word)) in dict1:
-        #         dict1[str(sorted(word))].append(word)
-        #     else:
-        #         dict1[str(sorted(word))] = [word]
-        # for key, val in dict1.items():
-        #     res.append(val)
-        # return res
-        ans = collections.defaultdict(list)
-        for s in strs:
-            count = [0] * 26
-            for c in s:
-                count[ord(c) - ord("a")] += 1
-            ans[tuple(count)].append(s)
-        return list(ans.values())
-       
+        word_dict = {}
+        result = []
 
-            
+        for word in strs:
+            sorted_word = tuple(sorted(word))
+            if sorted_word in word_dict:
+                word_dict[sorted_word].append(word)
+            else:
+                word_dict[sorted_word] = [word]
+        # print(word_dict)
+        for val in word_dict.values():
+            result.append(val)
+        # print(result)
+        return result
