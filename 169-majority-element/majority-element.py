6b98664b382bcca07e1e65159543a2c4b43f7ed0
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-05-21 14:10:49 -0700

    Time: 7 ms (57.50%) | Memory: 19.4 MB (51.60%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,3 +1,18 @@
+# class Solution:
+#     def majorityElement(self, nums: List[int]) -> int:
+#         count = 0
+#         element = -1
+#         for i in range(len(nums)):
+#             if count == 0:
+#                 element = nums[i]
+#             if nums[i] == element:
+#                 count+=1
+#             else:
+#                 count-=1
+            
+#         return element
+
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         count = 0
@@ -5,7 +20,7 @@ class Solution:
         for i in range(len(nums)):
             if count == 0:
                 element = nums[i]
-                # count=1
+                # count+=1
             if nums[i] == element:
                 count+=1
             else:
