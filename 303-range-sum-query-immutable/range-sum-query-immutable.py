a4c6ce504f9fd38772d228775ee645bd65343fcc
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-05-22 00:35:43 -0700

    Time: 5 ms (51.05%) | Memory: 21.9 MB (28.59%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.py
+++ 303-range-sum-query-immutable/range-sum-query-immutable.py
@@ -2,25 +2,17 @@ class NumArray:
 
     def __init__(self, nums: List[int]):
         self.nums = nums
-        self.dict1 = {}
-        self.total = sum(self.nums)
-        self.left_map = [self.nums[0]]
-        self.right_map = [0]*len(self.nums)
-        for i in range(1,len(self.nums)):
-            self.left_map.append(self.left_map[i-1] + self.nums[i])
-        # for i in range(len(self.nums)-2, -1, -1):
-        #     self.right_map[i] = self.right_map[i+1] + self.nums[i+1]
+
+        self.left_map = [0]
+        for i in range(len(self.nums)):
+            self.left_map.append(self.left_map[i] + self.nums[i])
         print(self.left_map)
-        # print(self.right_map)
-
-
-
 
     def sumRange(self, left: int, right: int) -> int:
-        if left == 0:
-            return self.left_map[right]
-        else:
-            return self.left_map[right] - self.left_map[left-1]
+        # if left == 0:
+        #     return self.left_map[right]
+        # else:
+        return self.left_map[right+1] - self.left_map[left]
 
 
 # Your NumArray object will be instantiated and called as such:
