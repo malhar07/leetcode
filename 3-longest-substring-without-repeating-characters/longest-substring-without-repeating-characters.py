ec4fc28cadb7a552aa12ef102db5f28ea28ca8a4
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-05-27 13:26:34 -0700

    Time: 15 ms (81.81%) | Memory: 18 MB (34.16%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,16 +1,31 @@
+# class Solution:
+#     def lengthOfLongestSubstring(self, s: str) -> int:
+#         dict1 = {}
+#         res = 0
+#         left = 0
+#         for i in range(len(s)):
+#             if s
+#             if s[i] in dict1:
+#                 res = max(res, i - dict1[s[i]])
+#                 left = i
+#                 while s[i] in dict1:
+#                     del dict1[s[left]]
+#                     left+=1
+#             # else:
+#             dict1[s[i]] = i
+#         return max(res,len(s)-left+1)
+
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        dict1 = {}
-        res = 0
+        char_index = {}
         left = 0
-        for ind in range(len(s)):
-            if s[ind] in dict1:
-                res = max(res, ind-left)
-                while s[ind] in dict1:
-                    del dict1[s[left]]
-                    left+=1
-            # else:
-            dict1[s[ind]] = ind
-        # print(ind)
-        return max(res, len(s) - left )
-        return res
+        max_length = 0
+
+        for right in range(len(s)):
+            if s[right] in char_index and char_index[s[right]] >= left:
+                left = char_index[s[right]] + 1
+            char_index[s[right]] = right
+            max_length = max(max_length, right - left + 1)
+
+        return max_length
+
