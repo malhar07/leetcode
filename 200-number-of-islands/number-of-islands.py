d49cb01615b5e422ae3cfb80a0122fe10fed72fe
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-05-30 17:26:06 -0700

    Time: 264 ms (37.25%) | Memory: 25.3 MB (29.97%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,45 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        islands = 0
+        dirs = [[0,1], [0,-1], [1,0], [-1,0]]
         visited = set()
-
-        def bfs(x,y):
-            q = deque([(x,y)])
-            visited.add((x,y))
-            directions = [[0,1],[1,0],[-1,0],[0,-1]]
-            while q:
-
-            
-                # for i in range(len(q)):
-                    # print("kkbkbkbkbkb")
-                x,y = q.popleft()
-                for dr, dc in directions:
-                    newx,newy = x+ dr, y+dc
-                    if 0<=newx<len(grid) and 0<=newy<len(grid[0]) and (newx,newy) not in visited and grid[newx][newy] == "1":
-                        print(newx, " ", newy)
-                        visited.add((newx, newy))
-                        q.append((newx, newy))
-                    
-
         
+        def bfs(r,c):
+            # print(visited)
+            q = deque([(r,c)])
 
+            while q:
+                r,c = q.popleft()
+
+                for dr,dc in dirs:
+                    newr, newc = r+dr, c+dc
+                    if 0<=newr<len(grid) and 0<=newc<len(grid[0]) and (newr,newc) not in visited and grid[newr][newc] == "1":
+                        # print(newr, " ", newc)
+                        q.append((newr,newc))
+                        visited.add((newr,newc))
+        count = 0
         for i in range(len(grid)):
-            for j in range (len(grid[0])):
+            for j in range(len(grid[0])):
                 if grid[i][j] == "1" and (i,j) not in visited:
-                    islands += 1
+                    print(i," ", j)
+                    visited.add((i,j))
                     bfs(i,j)
-        return islands
-
-
-
-        # def dfs(x,y):
-        #     # q = deque([(x,y)])
-        #     visited.add((x,y))
-
-        #     directions = [[0,1],[1,0],[-1,0],[0,-1]]
-        #     for dr, dc in directions:
-        #         newx,newy = x+ dr, y+dc
-        #         if 0<=newx<len(grid) and 0<=newy<len(grid[0]) and (newx,newy) not in visited and grid[newx][newy] == "1":       
-        #             visited.add((newx, newy))
-        #             dfs(newx, newy)
-                
+                    count+=1
+        return count
