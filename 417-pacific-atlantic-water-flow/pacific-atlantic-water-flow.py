a84fbbbc8f27d3c8bdf4f4b5abeb992eddeb736f
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-05-31 16:45:30 -0700

    Time: 31 ms (71.45%) | Memory: 19.3 MB (80.43%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,45 +1,35 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         dirs = [[0,1], [1,0], [0,-1], [-1,0]]
-        dp = {}
+        pacific = deque()
+        atlantic = deque()
 
-        def bfs(r1,c1):
-            q = deque([(r1,c1)])
-            flows = [False, False]
-            visited = set([(r1,c1)])
+        def bfs(que):
+            visited = set()
+            res = set()
 
-            while q:
-                r,c = q.popleft()
-                # print(r," ",c)
-                if r==0 or c == 0:
-                    flows[0] = True
-                if r == len(heights)-1 or c == len(heights[0])-1:
-                    flows[1] = True
-                if flows[0] == True and flows[1] == True:
-                    dp[(r1,c1)] = True
-                    return True
-                
+            while que:
+                r,c = que.popleft()
+                res.add((r,c))
+                visited.add((r,c))
 
-                for dr, dc in dirs:
+                for dr,dc in dirs:
                     newr, newc = r+dr, c+dc
-                    # if (newr,newc) in dp :
-                    #     if dp[(newr,newc)] == False:
-                    #         continue
-                    #     else:
-                    #         dp[(r1,c1)] = True
-                    #         return True
+                    if 0<=newr<len(heights) and 0<=newc<len(heights[0]) and heights[newr][newc] >= heights[r][c] and (newr,newc) not in visited:
+                        que.append((newr,newc))
+                        visited.add((newr,newc))
+            return res
 
-                    if 0<=newr<len(heights) and 0<=newc<len(heights[0]) and heights[newr][newc] <= heights[r][c] and (newr,newc) not in visited:
-                        q.append((newr,newc))
-                        visited.add((newr, newc))
-            dp[(r1,c1)] = False
-            return False
-        
-        res = []
-        
         for i in range(len(heights)):
-            for j in range(len(heights[0])):
-                if bfs(i,j):
-                    res.append([i,j])
-        return res
-                
+            pacific.append((i,0))
+            atlantic.append((i, len(heights[0])-1))
+        for i in range(len(heights[0])):
+            pacific.append((0,i))
+            atlantic.append((len(heights)-1, i))
+
+        
+        pacific = bfs(pacific)
+        atlantic = bfs(atlantic)
+
+        return list(atlantic & pacific)
+        
