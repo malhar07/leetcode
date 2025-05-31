f8e2695dd017be899d920a44147b71075a7ec65c
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-05-30 18:37:00 -0700

    Time: 51 ms (49.48%) | Memory: 22.3 MB (40.39%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.py
+++ 286-walls-and-gates/walls-and-gates.py
@@ -3,26 +3,26 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        visited = set()
-        q= deque([])
-        def bfs(q):
-            directions = [[0,1],[1,0],[0,-1],[-1,0]]
-            dist = 1
-            while q:
-                for i in range(len(q)):
-                    r,c = q.popleft()
-                    for dr,dc in directions:
-                        newr, newc = r+dr, c+dc
+        q = deque()
+        dirs = [[0,1], [1,0], [0,-1], [-1,0]]
 
-                        if 0<=newr<len(rooms) and 0<=newc<len(rooms[0]) and rooms[newr][newc] not in [0,-1] and (newr,newc) not in visited:
-                            rooms[newr][newc] = dist#min(dist, rooms[newr][newc])
-                            q.append((newr,newc))
-                            visited.add((newr,newc))
-                dist+=1
-
-        
         for i in range(len(rooms)):
             for j in range(len(rooms[0])):
                 if rooms[i][j] == 0:
                     q.append((i,j))
-        bfs(q)
+        
+        visited = set()
+        dist = 1
+        while q:
+            
+            for i in range(len(q)):
+                r,c = q.popleft()
+
+                for dr, dc in dirs:
+                    newr, newc = r+dr, c+dc
+
+                    if 0<=newr<len(rooms) and 0<=newc<len(rooms[0]) and (newr,newc) not in visited and rooms[newr][newc] not in [0,-1]:
+                        rooms[newr][newc] = dist
+                        q.append((newr,newc))
+                        visited.add((newr,newc))
+            dist+=1
