02a70ca684cff166c07bde1df46a5a892bdae7d1
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-05-30 17:12:10 -0700

    Time: 733 ms (88.60%) | Memory: 18.2 MB (36.25%) - LeetSync

--- 277-find-the-celebrity/find-the-celebrity.py
+++ 277-find-the-celebrity/find-the-celebrity.py
@@ -6,14 +6,20 @@ class Solution:
     def findCelebrity(self, n: int) -> int:
         candidate = 0
         index = 1
+        cache = {}
         while index < n:
             if knows(candidate, index):
+                cache[(candidate, index)] = True
                 candidate = index
+            else:
+                cache[(candidate,index)] = False
             index += 1
         
         def check_candidate(candidate):
             for i in range(n):
-                if i != candidate and (not knows(i,candidate) or knows(candidate,i)):
+                if i == candidate:
+                    continue
+                if not knows(i,candidate) or knows(candidate,i):
                     return -1
             return candidate
         return check_candidate(candidate)
