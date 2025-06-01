e7662ad2e370f9162f50b41328b5b4a3869efaf9
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-01 16:32:43 -0700

    Time: 11 ms (51.34%) | Memory: 19.1 MB (47.80%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,49 +1,35 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        # def first(arr, target):
-        #     left = 0
-        #     right = len(arr)-1
-        #     res = -1
-        #     while left<=right:
-        #         mid = left + (right-left)//2
-        #         if arr[mid] == target:
-        #             return mid
-        #         elif arr[mid] > target:
-        #             res =  mid
-        #             right = mid-1
-        #         else:
-        #             left = mid+1
-        #     if mid > 0 and ( abs(arr[mid-1]-target) <= abs(arr[mid] - target) ):
-        #         mid-=1
-        #     return mid
-        # mid = first(arr, x)
-        # left = mid
-        # right = mid
-        # for i in range(k-1):
-        #     if left == 0:
-        #         right+=1
-        mid = len(arr)-1
-        for ind, i in enumerate(arr):
-            if i == x:
-                mid = ind
-                break
-            if i > x:
-                mid = ind
-                if ind>0 and abs(x-arr[ind-1]) <= abs(x-arr[ind]):
-                    mid = ind-1
-                    break
-        left = right = mid
-        print(left)
+        left = 0
+        right = len(arr)-1
+        res = -1
 
+        while left<=right:
+            mid = left + (right-left)//2
+
+            if arr[mid] < x:
+                res = mid
+                left = mid+1
+            elif arr[mid] == x:
+                res = mid
+                right = mid-1
+            else:
+                right = mid-1
+        res = max(res,0)
+        mid = res
+        if mid+1<len(arr):
+            if abs(arr[mid] - x) > abs(arr[mid+1]-x):
+                mid = mid+1
+        
+        left = right = mid
         for i in range(k-1):
             if left == 0:
                 right+=1
             elif right == len(arr)-1:
                 left-=1
-            elif abs(x - arr[left-1]) <= abs(x-arr[right+1]):
-                left-=1
             else:
-                right+=1
+                if abs(arr[left-1]-x) <= abs(arr[right+1]-x):
+                    left-=1
+                else:
+                    right+=1
         return arr[left:right+1]
-
-            
