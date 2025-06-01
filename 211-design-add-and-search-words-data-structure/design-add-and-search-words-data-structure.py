a88005c29e32347bf115ab0692322346852ff063
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-01 15:11:42 -0700

    Time: 1150 ms (78.58%) | Memory: 65.7 MB (34.20%) - LeetSync

--- 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
+++ 211-design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.py
@@ -2,10 +2,11 @@ class Node:
     def __init__(self):
         self.children = {}
         self.word = False
-
 class WordDictionary:
+
     def __init__(self):
         self.root = Node()
+        
 
     def addWord(self, word: str) -> None:
         curr = self.root
@@ -14,65 +15,30 @@ class WordDictionary:
                 curr.children[c] = Node()
             curr = curr.children[c]
         curr.word = True
+        
 
     def search(self, word: str) -> bool:
-        def dfs(ind: int, curr: Node) -> bool:
-            if ind == len(word):
-                return curr.word
+        curr = self.root
 
+        def dfs(ind, curr):
+
+            if len(word) ==ind:
+                return curr.word
             c = word[ind]
+            
             if c != ".":
                 if c not in curr.children:
                     return False
                 return dfs(ind + 1, curr.children[c])
+                # curr = curr.children[c]
             else:
-                for child in curr.children.values():
-                    if dfs(ind + 1, child):
+                for child in curr.children:
+                    found = dfs(ind+1, curr.children[child])
+                    if found:
                         return True
                 return False
-
-        return dfs(0, self.root)
-
-# class Node:
-#     def __init__(self):
-#         self.children = {}
-#         self.word = False
-# class WordDictionary:
-
-#     def __init__(self):
-#         self.root = Node()
-        
-
-#     def addWord(self, word: str) -> None:
-#         curr = self.root
-#         for c in word:
-#             if c not in curr.children:
-#                 curr.children[c] = Node()
-#             curr = curr.children[c]
-#         curr.word = True
-        
-
-#     def search(self, word: str) -> bool:
-#         curr = self.root
-
-#         def dfs(ind, curr):
-
-#             if len(word) ==ind:
-#                 return curr.word
-#             c = word[ind]
-            
-#             if c != ".":
-#                 if c not in curr.children:
-#                     return False
-#                 curr = curr.children[c]
-#             else:
-#                 for child in curr.children:
-#                     found = dfs(ind+1, curr.children[child])
-#                     if found:
-#                         return True
-#                 return False
-#             return True
-#         return dfs(0,curr)
+            return True
+        return dfs(0,curr)
 
 
 # Your WordDictionary object will be instantiated and called as such:
