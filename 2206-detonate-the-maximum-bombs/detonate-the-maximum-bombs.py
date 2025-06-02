17eb1e99a44ae2842449526cbfc2ad0147a8042f
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-01 22:43:28 -0700

    Time: 366 ms (46.73%) | Memory: 18.1 MB (77.12%) - LeetSync

--- 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.py
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.py
@@ -1,37 +1,30 @@
 class Solution:
     def maximumDetonation(self, bombs: List[List[int]]) -> int:
         def dist(x1,y1,x2,y2):
-            return ((x1-x2)**2 + (y1-y2)**2)**0.5
-        adjlist = {}
+            return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+        adjlist = defaultdict(list)
         for i in range(len(bombs)):
-            adjlist[i] = []
-        for i in range(len(bombs)):
-            for j in range(len(bombs)):
-                if i==j:
-                    continue
-                if dist(bombs[i][0], bombs[i][1], bombs[j][0], bombs[j][1]) <= bombs[i][2]:
-                    adjlist[i].append(j)
+            for j in range((len(bombs))):
+                if i!=j:
+                    if dist(bombs[i][0], bombs[i][1], bombs[j][0], bombs[j][1]) <= bombs[i][2]:
+                        adjlist[i].append(j)
         
-
-        def bfs(i):
-            count = 1
-            q = deque()
-            q.append(i)
-            path = set()
-            path.add(i)
+        
+        def dfs(i):
+            visited = set([i])
+            q = deque([i])
 
             while q:
-                for i in range(len(q)):
-                    curr = q.popleft()
-                    for neigh in adjlist[curr]:
-                        if neigh not in path:
-                            q.append(neigh)
-                            path.add(neigh)
-                            count+=1
-            return count
+                curr = q.popleft()
+                for bomb in adjlist[curr]:
+                    if bomb not in visited:
+                        visited.add(bomb)
+                        q.append(bomb)
+            return len(visited)
 
         res = 0
+
         for i in range(len(bombs)):
-            res = max(res, bfs(i))
+            visited = set()
+            res = max(res,dfs(i))
         return res
-            
