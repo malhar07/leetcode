6b53bf2797dc2eb3bf02536c3b0ef64868e4252a
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-01 19:03:08 -0700

    Time: 7 ms (59.52%) | Memory: 18.8 MB (61.65%) - LeetSync

--- 735-asteroid-collision/asteroid-collision.py
+++ 735-asteroid-collision/asteroid-collision.py
@@ -1,24 +1,34 @@
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
-        stack = []
+        # stack = []
 
+        # for i in asteroids:
+        #     if not stack or not(i <0 and stack[-1]>0):
+        #         stack.append(i)
+        #     else:
+        #         while stack and (i<0 and stack[-1]>0):
+        #             curr = stack.pop()
+        #             if abs(i) > curr:
+        #                 curr = i
+        #             else:
+        #                 break
+        #         if abs(i)!=curr:
+        #                 stack.append(curr)
+        # return stack
+
+        stack = []
         for i in asteroids:
-            if not stack or not(i <0 and stack[-1]>0):
-                stack.append(i)
+            if i < 0:
+                if not stack or stack[-1] < 0:
+                    stack.append(i)
+                else:
+                    while stack and stack[-1]>0 and abs(i) > abs(stack[-1]):
+                        stack.pop()
+                    if not stack or stack[-1]<0:
+                        stack.append(i)
+                    elif stack and stack[-1] == abs(i):
+                        stack.pop()
             else:
-                while stack and (i<0 and stack[-1]>0):
-                    
-                    curr = stack.pop()
-                    if abs(i) > curr:
-                        curr = i
-                    else:
-                        break
-                    # elif abs(i) < curr:
-                    #     break                 
-                    # else:
-                    #     break
-                if abs(i)!=curr:
-                        stack.append(curr)
-            # print(stack)
+                stack.append(i)
         return stack
                     
