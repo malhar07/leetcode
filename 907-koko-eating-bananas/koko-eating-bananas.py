a5aa55384522eaf6318280ab7cd4f25e0b7f9b57
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-02 12:23:18 -0700

    Time: 239 ms (6.48%) | Memory: 18.8 MB (75.13%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,17 +1,19 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        piles.sort()
         left = 1
-        right = max(piles)
-        res = -1
+        right = sum(piles)
 
         while left <= right:
-            mid = (left+right)//2
+            print(left, " ", right)
             time = 0
-            for p in piles:
-                time+=(p-1)//mid + 1
-            if time <= h:
-                right = mid-1
-                res = mid
-            else:
+            mid = left+(right-left)//2
+            for num in piles:
+                time+=math.ceil(num/mid)
+            if time > h:
                 left = mid+1
+            else:
+                res = mid
+                right = mid-1
+            
         return res
