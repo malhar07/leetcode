2c7cb99e49f464ec98e1d76953e8e8d585ad544e
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-03 16:45:22 -0700

    Time: 150 ms (37.73%) | Memory: 32.2 MB (62.79%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -6,32 +6,15 @@
 #         self.right = right
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
-        self.res = 0
-
-
-        def helper(root, prev):
+        res = 0
+        def dfs(root,_max):
+            nonlocal res
             if not root:
-                return 0
-            # self.res += root.val >= prev
-            curr = root.val >= prev
-            left = helper(root.left, max(root.val, prev))
-            right = helper(root.right, max(root.val, prev))
-            return curr+ left + right
-        return helper(root, -math.inf)
-        # return self.res
-
-        # res = 0
-
-        # stack = [(root, -math.inf)]
-        # while stack:
-        #     curr,prev = stack.pop()
-        #     if curr.val >= prev:
-        #         res+=1
-        #     if curr.left:
-        #         stack.append((curr.left, max(curr.val,prev)))
-        #     if curr.right:
-        #         stack.append((curr.right, max(curr.val,prev)))
-        # return res
-
-            
-        
+                return
+            if root.val >= _max:
+                res+=1
+            _max = max(_max, root.val)
+            dfs(root.left, _max)
+            dfs(root.right, _max)
+        dfs(root,-math.inf)
+        return res
