9f814f52ab5e3e3e7f90084657ad45193b6c3282
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-03 15:15:36 -0700

    Time: 52 ms (87.60%) | Memory: 21.1 MB (47.71%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -7,27 +7,22 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # def helper(root):
-        #     if not root:
-        #         return 
-        #     if root.val > p.val and root.val >q.val:
-        #         helper(root.left)
-        #     elif root.val < p.val and root.val < q.val:
-        #         helper(root.right)
-        #     else:
-        #         self.res = root
-        # self.res = []
-        # helper(root)
-        # return self.res
-
-        stack = [root]
-        while stack:
-            curr = stack.pop()
-            if curr.val > p.val and curr.val > q.val:
-                stack.append(curr.left)
-            elif curr.val < p.val and curr.val < q.val:
-                stack.append(curr.right)
-            else:
-                return curr
+        res = [-1]
+        def dfs(root):
+            if not root:
+                return False
             
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            count = (root.val == p.val or root.val == q.val) + left + right
+            if count>1:
+                res[0] = root
+                return True
+
+            if root.val == p.val or root.val == q.val:
+                return True
+            return left or right
+        dfs(root)
+        return res[0]
             
