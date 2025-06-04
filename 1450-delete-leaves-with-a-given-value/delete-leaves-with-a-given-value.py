decddab3c5871820265f3d485dd93d93d2262239
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-03 20:49:50 -0700

    Time: 7 ms (8.42%) | Memory: 18.8 MB (8.34%) - LeetSync

--- 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
@@ -7,34 +7,45 @@
 class Solution:
     def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
 
-        def dfs(root):
-            print("kkk")
-            if not root:
-                return True
-            left = dfs(root.left)
-            right = dfs(root.right)
+        # def dfs(root):
+        #     print("kkk")
+        #     if not root:
+        #         return True
+        #     left = dfs(root.left)
+        #     right = dfs(root.right)
 
-            if left and right and root.val == target:
-                root.val = -1
-                return True
-            return False
-        dfs(root)
-        if root.val == -1:
-            return None
+        #     if left and right and root.val == target:
+        #         root.val = -1
+        #         return True
+        #     return False
+        # dfs(root)
+        # if root.val == -1:
+        #     return None
 
-        def deleteNodes(root):
-            if not root:
-                return 
-            if root.left and root.left.val == -1:
-                root.left = None
-            else:
-                deleteNodes(root.left)
-            if root.right and root.right.val == -1:
-                root.right = None
-            else:
-                deleteNodes(root.right)
-            return root
+        # def deleteNodes(root):
+        #     if not root:
+        #         return 
+        #     if root.left and root.left.val == -1:
+        #         root.left = None
+        #     else:
+        #         deleteNodes(root.left)
+        #     if root.right and root.right.val == -1:
+        #         root.right = None
+        #     else:
+        #         deleteNodes(root.right)
+        #     return root
             
 
-        return deleteNodes(root)
+        # return deleteNodes(root)
+        def dfs(root):
+            if not root:
+                return None
+            
+            root.left = dfs(root.left)
+            root.right = dfs(root.right)
 
+            if not root.left and not root.right and root.val == target:
+                print(root.val)
+                return None
+            return root
+        return dfs(root)
