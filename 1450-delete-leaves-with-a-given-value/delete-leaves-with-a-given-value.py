df71f14e915860c2a410ba9cf375a2de254fa5b3
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-03 20:41:49 -0700

    Time: 4 ms (22.39%) | Memory: 18.3 MB (45.10%) - LeetSync

---
+++ 1450-delete-leaves-with-a-given-value/delete-leaves-with-a-given-value.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+
+        def dfs(root):
+            print("kkk")
+            if not root:
+                return True
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            if left and right and root.val == target:
+                root.val = -1
+                return True
+            return False
+        dfs(root)
+        if root.val == -1:
+            return None
+
+        def deleteNodes(root):
+            if not root:
+                return 
+            if root.left and root.left.val == -1:
+                root.left = None
+            else:
+                deleteNodes(root.left)
+            if root.right and root.right.val == -1:
+                root.right = None
+            else:
+                deleteNodes(root.right)
+            return root
+            
+
+        return deleteNodes(root)
