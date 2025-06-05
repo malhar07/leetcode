f61537e4f9f733e030e8cd9390f872e2030a95b5
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-05 11:53:16 -0700

    Time: 3 ms (94.30%) | Memory: 18 MB (53.83%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,20 +1,28 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         res = []
-        cnt = Counter(nums)
+        nums.sort()
 
-        def helper(perm, cnt):
-            if len(perm) == len(nums):
-                res.append(perm[:])
-                return
-
-            for i in cnt:
-                if cnt[i] >0:
-                    perm.append(i)
-                    cnt[i] -= 1
-                    helper(perm, cnt)
-                    perm.pop()
-                    cnt[i]+=1
-                
-        helper([], cnt)
+        def helper(arr, used):
+            
+            if len(arr) == len(nums):
+                res.append(arr[:])
+                return 
+            # if ind >= len(nums):
+            #     return 
+            visited = set()
+            for i in range(len(nums)):
+                if not used[i] and nums[i] not in visited:
+                    used[i] = True
+                    visited.add(nums[i])
+                    arr.append(nums[i])
+                    helper(arr, used)
+                    arr.pop()
+                    used[i] = False
+                else:
+                    continue
+        
+        helper([],[False]*len(nums))
         return res
+            
+        
