2c4e1ba91450299e94dc458405070c178bdc3383
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-05 14:52:08 -0700

    Time: 0 ms (100.00%) | Memory: 17.6 MB (78.11%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,8 +1,8 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n < 3:
-            return n
-        arr = [0]*(n+1)
+        # if n < 3:
+        #     return n
+        arr = [0]*(n+2)
 
         arr[1] = 1
         arr[2] = 2
