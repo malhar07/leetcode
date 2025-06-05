40579208de1873540af058179e23263290836282
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-05 01:19:27 -0700

    Time: 145 ms (35.71%) | Memory: 59.7 MB (28.99%) - LeetSync

--- 77-combinations/combinations.py
+++ 77-combinations/combinations.py
@@ -1,11 +1,11 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        # nums = [ i for i in range(1,n+1)]
         res = []
         def helper(arr, ind):
+            if len(arr) == k:
+                res.append(arr[:])
+                return
             if ind > n:
-                if len(arr) == k:
-                    res.append(arr[:])
                 return
             
             arr.append(ind)
@@ -14,19 +14,3 @@ class Solution:
             helper(arr, ind+1)
         helper([],1)
         return res
-
-
-        # nums = [ i for i in range(1,n+1)]
-        # res = []
-        # def helper(arr, ind):
-        #     if ind == len(nums):
-        #         if len(arr) == k:
-        #             res.append(arr[:])
-        #         return
-            
-        #     arr.append(nums[ind])
-        #     helper(arr,ind+1)
-        #     arr.pop()
-        #     helper(arr, ind+1)
-        # helper([],0)
-        # return res
