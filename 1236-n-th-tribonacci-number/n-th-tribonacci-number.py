37e4ee0343b61e98c8da9f5806ad972d8624bc58
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-25 23:37:17 -0700

    Time: 0 ms (100.00%) | Memory: 17.8 MB (47.97%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,14 +1,12 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        memo = [-1]*(n+1)
-        def tribo(n):
-            if n == 0:
-                return 0
-            if n == 1 or n == 2:
-                return 1
-            if memo[n]==-1:
-                memo[n] = tribo(n-1)+tribo(n-2)+tribo(n-3)
-
-            
-            return memo[n]
-        return tribo(n)
+        if n == 0:
+            return 0
+        if n == 1 or n == 2:
+            return 1
+        tribo = [0,1,1]
+        for i in range(3,n+1):
+            curr = sum(tribo)
+            tribo[0], tribo[1] = tribo[1], tribo[2]
+            tribo[2] = curr
+        return tribo[2]
