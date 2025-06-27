ab411a0d0c2732d55c62c145d9977a11d66676de
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-26 18:15:09 -0700

    Time: 11 ms (50.02%) | Memory: 19.9 MB (42.35%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.py
+++ 740-delete-and-earn/delete-and-earn.py
@@ -1,16 +1,22 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
+        
         count = Counter(nums)
-        # nums.sort()
-        dp = [0]*(max(nums)+2)
-        dp[max(nums)] = count[max(nums)]*max(nums)
-        [1,3,6]
+        nums = list(set(nums))
+        nums.sort()
 
-        # dp = [0]*(max(nums)+1)
-        for i in range(max(nums)-1, 0, -1):
-            if i not in count:
-                curr_points = 0
-            else:
-                curr_points = count[i]*i
-            dp[i] = max(dp[i+1], curr_points+dp[i+2])
-        return dp[1]
+        dp = [0]*len(nums)
+        dp[0] = nums[0]*count[nums[0]]
+
+        [2,3,4]
+
+        for i in range(1,len(nums)):
+            prev = dp[i-1]
+            if nums[i] == nums[i-1]+1:
+                if i-2>=0:
+                    prev = dp[i-2]
+                else:
+                    prev = 0
+            dp[i] = max((nums[i]*count[nums[i]] + prev), dp[i-1])
+        
+        return dp[-1]
