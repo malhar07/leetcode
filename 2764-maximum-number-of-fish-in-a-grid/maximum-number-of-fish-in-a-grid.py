eb574505bddb7ba6e6592dd30a6526df0877a80a
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-29 23:21:54 -0700

    Time: 47 ms (46.19%) | Memory: 18 MB (39.06%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
@@ -4,19 +4,22 @@ class Solution:
         visited = set()
         dirs = [[0,1], [1,0], [-1,0], [0,-1]]
 
+        def is_valid_neighbor(newr, newc):
+            if 0<=newr<len(grid) and 0<=newc<len(grid[0]) and grid[newr][newc]>0 and (newr,newc) not in visited:
+                return True
+            return False
+
 
         def dfs(r,c):
             visited.add((r,c))
             total = grid[r][c]
             for dr, dc in dirs:
                 newr, newc = r+dr, c+dc
-                if 0<=newr<len(grid) and 0<=newc<len(grid[0]) and grid[newr][newc]>0 and (newr,newc) not in visited:
+                if is_valid_neighbor(newr, newc):
+                
                     total+=dfs(newr, newc)
             return total 
 
-
-
-        
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] > 0 and (r,c) not in visited:
