71153db6e063549c5e13f180d5ae71b2f88345ef
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-29 23:19:55 -0700

    Time: 43 ms (55.02%) | Memory: 18.1 MB (39.06%) - LeetSync

---
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findMaxFish(self, grid: List[List[int]]) -> int:
+        max_count = 0
+        visited = set()
+        dirs = [[0,1], [1,0], [-1,0], [0,-1]]
+
+
+        def dfs(r,c):
+            visited.add((r,c))
+            total = grid[r][c]
+            for dr, dc in dirs:
+                newr, newc = r+dr, c+dc
+                if 0<=newr<len(grid) and 0<=newc<len(grid[0]) and grid[newr][newc]>0 and (newr,newc) not in visited:
+                    total+=dfs(newr, newc)
+            return total 
+
+
+
+        
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] > 0 and (r,c) not in visited:
+                    max_count = max(max_count, dfs(r,c))
+        return max_count
