2771a05f7d307bd5ba9b4e27372f3dfd7e06681b
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-30 00:32:06 -0700

    Time: 47 ms (53.73%) | Memory: 22.2 MB (39.85%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.py
+++ 286-walls-and-gates/walls-and-gates.py
@@ -3,26 +3,28 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        q = deque()
         dirs = [[0,1], [1,0], [0,-1], [-1,0]]
+        q = deque()
 
         for i in range(len(rooms)):
             for j in range(len(rooms[0])):
                 if rooms[i][j] == 0:
                     q.append((i,j))
         
-        visited = set()
-        dist = 1
-        while q:
-            
-            for i in range(len(q)):
-                r,c = q.popleft()
+        def bfs():
+            visited = set()
+            curr_distance = 1
 
-                for dr, dc in dirs:
-                    newr, newc = r+dr, c+dc
+            while q:
+                for _ in range(len(q)):
+                    r,c = q.popleft()
 
-                    if 0<=newr<len(rooms) and 0<=newc<len(rooms[0]) and (newr,newc) not in visited and rooms[newr][newc] not in [0,-1]:
-                        rooms[newr][newc] = dist
-                        q.append((newr,newc))
-                        visited.add((newr,newc))
-            dist+=1
+                    for dr, dc in dirs:
+                        newr, newc = r+dr, c+dc
+                        if 0<=newr<len(rooms) and 0<=newc<len(rooms[0]) and rooms[newr][newc] > 0 and (newr,newc) not in visited:
+                            rooms[newr][newc] = curr_distance
+                            visited.add((newr,newc))
+                            q.append((newr,newc))
+                curr_distance += 1
+        bfs()
+        # return 
