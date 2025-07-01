3a0314f91a8d346344dd2822dfdd9801872cd620
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-30 20:33:22 -0700

    Time: 224 ms (39.11%) | Memory: 19.4 MB (42.68%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,29 +1,26 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        
+        n = len(grid)
+        q = deque()
+        visited = set()
 
-        def bfs():
-            q = deque()
-            visited = set()
+        if grid[0][0]!= 0 :
+            return -1
 
-            q.append((0,0,1))
-            visited.add((0,0))
-            directions = [[0,1], [0,-1], [1,0], [-1,0], [1,1], [1,-1], [-1,1], [-1,-1]]
-            while q:
-                for i in range(len(q)):
-                    r,c, moves = q.popleft()
-                    print(r," ", c)
-                    if r == len(grid)-1 and c == len(grid)-1:
-                        return moves
-                    for dr, dc in directions:
-                        if 0<=r+dr<len(grid) and 0<=c+dc<len(grid) and (r+dr, c+dc) not in visited and grid[r+dr][c+dc] == 0:
-                            # print(r+dr, " ", c+dc)
-                            q.append((r+dr, c+dc, moves+1))
-                            visited.add((r+dr, c+dc))
-            return -1
-        
-        if grid[0][0]!= 0:
-            print(".vevev")
-            return -1
-        return bfs()
-                
+        q.append((0,0,1))
+        visited.add((0,0))
+        directions = [[0,1], [0,-1], [1,0], [-1,0], [1,1], [1,-1], [-1,1], [-1,-1]]
+        while q:
+            for _ in range(len(q)):
+                r,c,path_len = q.popleft()
+                if r == n-1 and c == n-1:
+                    return path_len
+
+                for dr, dc in directions:
+                    newr, newc = r+dr, c+dc
+                    if 0<=newr<n and 0<=newc<n and (newr,newc) not in visited and grid[newr][newc]==0:
+                        # if newr == n-1 and newc == n-1:
+                        #     return path_len+1
+                        visited.add((newr,newc))
+                        q.append((newr,newc,path_len+1))
+        return -1
