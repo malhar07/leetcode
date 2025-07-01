5009af55f0c773d74b96ca4821c8f31772fc1990
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-30 20:36:26 -0700

    Time: 199 ms (58.21%) | Memory: 19.5 MB (28.28%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -4,23 +4,20 @@ class Solution:
         q = deque()
         visited = set()
 
-        if grid[0][0]!= 0 :
+        if grid[0][0]!= 0 or grid[n-1][n-1]!=0:
             return -1
 
         q.append((0,0,1))
         visited.add((0,0))
         directions = [[0,1], [0,-1], [1,0], [-1,0], [1,1], [1,-1], [-1,1], [-1,-1]]
         while q:
-            for _ in range(len(q)):
-                r,c,path_len = q.popleft()
-                if r == n-1 and c == n-1:
-                    return path_len
+            r,c,path_len = q.popleft()
+            if r == n-1 and c == n-1:
+                return path_len
 
-                for dr, dc in directions:
-                    newr, newc = r+dr, c+dc
-                    if 0<=newr<n and 0<=newc<n and (newr,newc) not in visited and grid[newr][newc]==0:
-                        # if newr == n-1 and newc == n-1:
-                        #     return path_len+1
-                        visited.add((newr,newc))
-                        q.append((newr,newc,path_len+1))
+            for dr, dc in directions:
+                newr, newc = r+dr, c+dc
+                if 0<=newr<n and 0<=newc<n and (newr,newc) not in visited and grid[newr][newc]==0:
+                    visited.add((newr,newc))
+                    q.append((newr,newc,path_len+1))
         return -1
