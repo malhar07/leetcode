515eede5d0481f9afcb253f8e68b14d314fbc075
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-06-30 18:16:03 -0700

    Time: 35 ms (73.36%) | Memory: 23.6 MB (75.58%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.py
+++ 820-find-eventual-safe-states/find-eventual-safe-states.py
@@ -1,34 +1,53 @@
+# class Solution:
+#     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+#         adjlist = {node:[] for node in range(len(graph))}
+#         terminal = set()
+#         for node, nei in enumerate(graph):
+#             for n in nei:
+#                 adjlist[node].append(n)
+#         for i in range(len(graph)):
+#             if adjlist[i] == []:
+#                 terminal.add(i)
+#         path = set()
+#         def dfs(node):
+#             if node in terminal:
+#                 return True
+#             if node in path:
+#                 return False
+#             path.add(node)
+#             isSafe = True
+#             for nei in adjlist[node]:
+#                 isSafe = dfs(nei) and isSafe
+#                 if not isSafe:
+#                     return False
+#             path.remove(node)
+#             if isSafe:
+#                 terminal.add(node)
+#             return isSafe
+#         res = []
+#         for i in range(len(graph)):
+#             if i in terminal:
+#                 res.append(i)
+#             elif dfs(i):
+#                 res.append(i)
+#         return res
+        
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        adjlist = {node:[] for node in range(len(graph))}
-        terminal = set()
-        for node, nei in enumerate(graph):
-            for n in nei:
-                adjlist[node].append(n)
-        for i in range(len(graph)):
-            if adjlist[i] == []:
-                terminal.add(i)
-        path = set()
+        n = len(graph)
+        state = [0] * n  # 0 = unvisited, 1 = visiting, 2 = safe
+
         def dfs(node):
-            if node in terminal:
-                return True
-            if node in path:
-                return False
-            path.add(node)
-            isSafe = True
-            for nei in adjlist[node]:
-                isSafe = dfs(nei) and isSafe
-                if not isSafe:
+            if state[node] == 1:
+                return False  # cycle detected
+            if state[node] == 2:
+                return True   # already marked safe
+
+            state[node] = 1  # mark as visiting
+            for nei in graph[node]:
+                if not dfs(nei):
                     return False
-            path.remove(node)
-            if isSafe:
-                terminal.add(node)
-            return isSafe
-        res = []
-        for i in range(len(graph)):
-            if i in terminal:
-                res.append(i)
-            elif dfs(i):
-                res.append(i)
-        return res
-        
+            state[node] = 2  # mark as safe
+            return True
+
+        return [i for i in range(n) if dfs(i)]
