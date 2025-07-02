ec8fd21f75bc41a77d6622fc038804ffa1204ff5
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-07-02 14:21:49 -0700

    Time: 43 ms (68.77%) | Memory: 30.6 MB (59.52%) - LeetSync

--- 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
+++ 1350-remove-sub-folders-from-the-filesystem/remove-sub-folders-from-the-filesystem.py
@@ -1,10 +1,9 @@
 class Solution:
     def removeSubfolders(self, folder: List[str]) -> List[str]:
-        folder_set = set()
-        folder.sort()
-
+        folder_set = set(folder)
+        res = []
         for f in folder:
-            path = f.split('/')[1:]
+            path = f.split('/')[1:-1]
             sub_folder = ""
             parent_folder_present = False
             for file in path:
@@ -13,5 +12,5 @@ class Solution:
                     parent_folder_present = True
                     break
             if not parent_folder_present:
-                folder_set.add(f)
-        return list(folder_set)
+                res.append(f)
+        return list(res)
