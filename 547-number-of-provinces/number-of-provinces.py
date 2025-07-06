6213e75f707629db3ccab2b2610ebe1ba3b06635
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-07-06 01:15:09 -0700

    Time: 10 ms (28.68%) | Memory: 19 MB (58.37%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,8 +1,14 @@
 class UnionFind:
     def __init__(self, n):
-        self.parent = {i:i for i in range(n)}
-        self.rank = {i:0 for i in range(n)}
+        # self.parent = {i:i for i in range(n)}
+        # self.rank = {i:0 for i in range(n)}
+        self.parent = {}
+        self.rank = {}
     def find(self, node):
+        if node not in self.parent:
+            self.parent[node] = node
+            self.rank[node] = 0
+
         if self.parent[node] != node:
             self.parent[node] = self.find(self.parent[node])
         return self.parent[node]
@@ -14,7 +20,7 @@ class UnionFind:
         else:
             if self.rank[root1] > self.rank[root2]:
                 self.parent[root2] = root1
-            if self.rank[root2] > self.rank[root1]:
+            elif self.rank[root2] > self.rank[root1]:
                 self.parent[root1] = root2
             else:
                 self.parent[root2] = root1
