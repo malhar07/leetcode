45c7cfa8df4ecb5b64e2edb8a90cbd31a09bbc75
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-07-06 14:14:15 -0700

    Time: 1593 ms (7.43%) | Memory: 310.1 MB (10.84%) - LeetSync

---
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        memo = {} #(ind1,ind2) -> LCS
+        
+        def dfs(text1, text2, ind1, ind2):
+            if ind1 == len(text1) or ind2 == len(text2):
+                return 0
+            if (ind1,ind2) in memo:
+                return memo[(ind1,ind2)]
+            else:
+                lcs = 0
+
+                if text1[ind1] == text2[ind2]:
+                    lcs =  1 + dfs(text1, text2, ind1+1, ind2+1)
+                else:
+                    lcs =  max(dfs(text1,text2,ind1+1,ind2), dfs(text1,text2, ind1, ind2+1))
+                memo[(ind1,ind2)] = lcs
+                return memo[(ind1,ind2)]
+        return dfs(text1, text2, 0, 0)
