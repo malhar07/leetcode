82158f467237bcfd9c76e0a7c9cb2139b34f0d5d
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-07-05 20:39:18 -0700

    Time: 316 ms (60.90%) | Memory: 20.4 MB (24.21%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.py
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.py
@@ -2,26 +2,23 @@ class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         rows, cols = len(heights), len(heights[0])
         min_heap = []
-        heapq.heappush(min_heap,[0,0,0])
         visited = set()
+        heapq.heappush(min_heap,[0,0,0]) #[effort,r,c]
 
         directions = [[0,1], [1,0], [0,-1], [-1,0]]
 
-        def bfs():
+        while min_heap:
+            effort, r, c = heapq.heappop(min_heap)
+            if r == rows-1 and c == cols-1:
+                return effort
 
-            while min_heap:
-                effort, r,c = heapq.heappop(min_heap)
-                if (r, c) in visited:
-                    continue
-                visited.add((r,c))
-                if r == rows-1 and c == cols-1:
-                    return effort
-                for dr, dc in directions:
-                    newr, newc = r+dr, c+dc
-                    if 0<=newr<rows and 0<=newc<cols and (newr,newc) not in visited:
-                        max_effort = max(effort, abs(heights[r][c] - heights[newr][newc]))
-                        heapq.heappush(min_heap, [max_effort,newr,newc])
-                        
-        return bfs()
+            if (r,c) in visited:
+                continue
+            visited.add((r,c))
 
-
+            for dr, dc in directions:
+                newr, newc = r+dr, c+dc
+                if 0<=newr<rows and 0<=newc<cols and (newr,newc) not in visited:
+                    new_effort = max(effort, abs(heights[r][c] - heights[newr][newc]))
+                    heapq.heappush(min_heap, [new_effort, newr, newc])
+                
