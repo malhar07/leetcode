7c0544bfca8782c37c0f27e7b83f7b836f5f833f
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-07-05 20:50:46 -0700

    Time: 1827 ms (5.02%) | Memory: 21.2 MB (9.12%) - LeetSync

--- 744-network-delay-time/network-delay-time.py
+++ 744-network-delay-time/network-delay-time.py
@@ -1,25 +1,45 @@
-class Solution:
-    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        network = {i:[] for i in range(1,n+1)}
-        min_heap = []
+# class Solution:
+#     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        # network = {i:[] for i in range(1,n+1)}
+        # min_heap = []
 
-        for n1, n2, time in times:
-            network[n1].append([n2,time])
+        # for n1, n2, time in times:
+        #     network[n1].append([n2,time])
         
-        heapq.heappush(min_heap, [0,k])
-        visited = set()
+        # heapq.heappush(min_heap, [0,k])
+        # visited = set()
 
-        res = -1
+        # res = -1
 
-        while min_heap:
-            time, node  = heapq.heappop(min_heap)
+        # while min_heap:
+        #     time, node  = heapq.heappop(min_heap)
             
-            if node in visited:
-                continue
-            res = max(res, time)
-            visited.add(node)
+        #     if node in visited:
+        #         continue
+        #     res = max(res, time)
+        #     visited.add(node)
 
-            for nei, t in network[node]:
-                if nei not in visited:
-                    heapq.heappush(min_heap,[time+t, nei])
-        return res if len(visited) == n else -1
+        #     for nei, t in network[node]:
+        #         if nei not in visited:
+        #             heapq.heappush(min_heap,[time+t, nei])
+        # return res if len(visited) == n else -1
+
+class Solution:
+    def networkDelayTime(self, times, n, k):
+        graph = defaultdict(list)
+        for u, v, w in times:
+            graph[u].append((v, w))
+        
+        min_time = [float('inf')] * (n + 1)
+        min_time[k] = 0
+        
+        def dfs(node, elapsed):
+            for nei, wt in graph[node]:
+                if elapsed + wt < min_time[nei]:
+                    min_time[nei] = elapsed + wt
+                    dfs(nei, elapsed + wt)
+
+        dfs(k, 0)
+        
+        res = max(min_time[1:])  # skip 0 index
+        return res if res != float('inf') else -1
