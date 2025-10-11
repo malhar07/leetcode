b5cb4b1d64222324dbeb18807b8a114745e656aa
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-10-11 19:05:24 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (56.89%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -6,31 +6,39 @@
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         carry = 0
-        head = ListNode()
-        temp = head
+        dummy = ListNode(0)
+        head = dummy
+
         while l1 and l2:
-            add = l1.val+l2.val+carry
-            carry = add>9
-            add = add%10
+            _add = l1.val+l2.val+carry
+            carry = _add//10
+            _add = _add%10
 
-            new_node = ListNode(add)
-            temp.next = new_node
-            temp = temp.next
+            head.next = ListNode(_add)
+            head=head.next
             l1 = l1.next
-            l2 = l2.next
-        if not l1:
-            l1 = l2
-        while l1:
-            add = l1.val+carry
-            carry = add>9
-            add = add%10
+            l2=l2.next
+    
+        if l1:
+            while l1:
+                _add = l1.val+carry
+                carry = _add//10
+                _add = _add%10
 
-            new_node = ListNode(add)
-            temp.next = new_node
-            temp = temp.next
-            l1 = l1.next
-        if carry:
-            temp.next = ListNode(1)
-        return head.next
+                head.next = ListNode(_add)
+                head=head.next
+                l1 = l1.next
 
-
+        if l2:
+            while l2:
+                _add = l2.val+carry
+                carry = _add//10
+                _add = _add%10
+                
+                head.next = ListNode(_add)
+                head=head.next
+                l2 = l2.next
+        if carry > 0:
+            head.next = ListNode(1)
+        return dummy.next
+        
