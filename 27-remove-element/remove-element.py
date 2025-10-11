cb355cc885809d121c8440a47e4b3ea876a15da6
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-10-11 18:03:40 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (52.78%) - LeetSync

--- 27-remove-element/remove-element.py
+++ 27-remove-element/remove-element.py
@@ -1,10 +1,11 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        write = 0
-        ind = 0 
-        while ind < len(nums):
-            if nums[ind] != val:
-                nums[ind], nums[write] = nums[write], nums[ind]
-                write +=1
-            ind+=1
-        return write
+        swap_ind = len(nums)-1
+        ind = 0
+        while ind<=swap_ind:
+            if nums[ind] == val:
+                nums[ind], nums[swap_ind] = nums[swap_ind], nums[ind]
+                swap_ind -= 1
+            else:
+                ind+=1
+        return swap_ind+1
