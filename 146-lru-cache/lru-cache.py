c6cc48808ca0e450c53f238ad4098f3ef73bbaa4
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-10-12 16:24:34 -0400

    Time: 170 ms (22.83%) | Memory: 78 MB (72.59%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,50 +1,61 @@
-class Node:
-    def __init__(self, key, val):
+class node:
+    def __init__(self,key,val):
         self.key = key
         self.val = val
-        self.Next = None
+        self.next = None
         self.prev = None
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.dict1 = {}
-        self.head = Node(0,0)
-        self.tail = Node(0,0)
+        self.lru = {}
+        self.head = node(0,0)
+        self.tail = node(0,0)
         self.head.next = self.tail
         self.tail.prev = self.head
 
-
+        
 
     def get(self, key: int) -> int:
-        if key in self.dict1:
-            node = self.dict1[key]
-            self.remove(self.dict1[key])
-            self.add(node)
-            return node.val
+        if key in self.lru:
+            v = self.lru[key].val
+            self.remove(key)
+            self.add(key, v)
+            return v
         return -1
+        
 
     def put(self, key: int, value: int) -> None:
-        if key in self.dict1:
-            self.remove(self.dict1[key])
-        elif len(self.dict1) >= self.capacity:
-            k = self.tail.prev.key
-            del self.dict1[k]
-            self.remove(self.tail.prev)
-        node = Node(key, value)
-        self.add(node)
-        self.dict1[key] = node
+        if key in self.lru:
+            self.lru[key].val = value
+            self.remove(key)
+            self.add(key,value)
+        else:
+            if len(self.lru) >= self.capacity:
+                least = self.tail.prev.key
+                self.remove(least)
+                del self.lru[least]
+            #new_node = node(key, value)
+            new_node = self.add(key, value)
+            self.lru[key] = new_node
+        
+
     
-    def add(self,node):
-        node.next = self.head.next
-        node.prev = self.head
-        self.head.next = node
-        node.next.prev = node
-    
-    def remove(self,node):
-        nxt, prev = node.next, node.prev
-        prev.next = nxt
-        nxt.prev = prev
+    def add(self, k, v):
+        nn = node(k,v)
+        nn.next = self.head.next
+        nn.prev = self.head
+        self.head.next = nn
+        nn.next.prev = nn
+
+        self.lru[k] = nn
+
+        return nn
+
+    def remove(self, key):
+        nn = self.lru[key]
+        nn.next.prev = nn.prev
+        nn.prev.next = nn.next
 
 
 
