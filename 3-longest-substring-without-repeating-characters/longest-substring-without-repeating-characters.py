397a194222cefa3d017bb06ec10a7bb1cb4b4a5d
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-10-13 20:21:30 -0400

    Time: 19 ms (53.29%) | Memory: 17.8 MB (52.70%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -1,31 +1,22 @@
-# class Solution:
-#     def lengthOfLongestSubstring(self, s: str) -> int:
-#         dict1 = {}
-#         res = 0
-#         left = 0
-#         for i in range(len(s)):
-#             if s
-#             if s[i] in dict1:
-#                 res = max(res, i - dict1[s[i]])
-#                 left = i
-#                 while s[i] in dict1:
-#                     del dict1[s[left]]
-#                     left+=1
-#             # else:
-#             dict1[s[i]] = i
-#         return max(res,len(s)-left+1)
-
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        char_index = {}
         left = 0
-        max_length = 0
+        right = 0
+        curr_longest = 0
+        longest_sub = 0
+        chars = set()
+        while right<len(s):
+            if s[right] in chars:
+                longest_sub = max(longest_sub, curr_longest)
+                while s[left] != s[right]:
+                    chars.remove(s[left])
+                    left+=1
+                    curr_longest-=1
+                chars.remove(s[left])
+                left+=1
+                curr_longest-=1
 
-        for right in range(len(s)):
-            if s[right] in char_index and char_index[s[right]] >= left:
-                left = char_index[s[right]] + 1
-            char_index[s[right]] = right
-            max_length = max(max_length, right - left + 1)
-
-        return max_length
-
+            curr_longest +=1
+            chars.add(s[right])
+            right+=1
+        return max(longest_sub, curr_longest)
