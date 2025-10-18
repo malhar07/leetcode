10b74fdcd5b9f7e8b77a6ecd91e932e576436f99
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-10-18 13:41:48 -0400

    Time: 142 ms (26.80%) | Memory: 31.7 MB (5.98%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.py
@@ -1,43 +1,29 @@
 class NumMatrix:
 
     def __init__(self, matrix: List[List[int]]):
-        self.matrix = matrix
-        self.prefix = copy.deepcopy(matrix)
+        rows = len(matrix) + 1
+        cols = (len(matrix[0]) if matrix else 0) + 1
+        self.new_mat = [[0] * cols for _ in range(rows)]
 
-
-
-        for i in range(len(self.prefix)):
-            for j in range(1, len(self.prefix[0])):
-                self.prefix[i][j] += self.prefix[i][j-1]
-        print(self.prefix)
-        for j in range(len(self.prefix[0])):
-            for i in range(1, len(self.prefix)):
-                self.prefix[i][j] += self.prefix[i-1][j]
-
-        print(self.prefix)
+        rows = len(matrix)
+        cols = len(matrix[0])
+        for i in range(rows-1,-1,-1):
+            for j in range(cols-1,-1,-1):
+                self.new_mat[i][j] = matrix[i][j]+self.new_mat[i][j+1]+self.new_mat[i+1][j]-self.new_mat[i+1][j+1]
+        print(self.new_mat)
         
-
-
-
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        if col1 > 0:
-            left = self.prefix[row2][col1-1]
-        else:
-            left = 0
-        if row1>0:
-            up = self.prefix[row1-1][col2]
-        else:
-            up = 0
-        if row1>0 and col1>0:
-            corner = self.prefix[row1-1][col1-1]
-        else:
-            corner = 0
-        total = self.prefix[row2][col2] - left - up + corner
-        return total
-        
+        return self.new_mat[row1][col1]-self.new_mat[row2+1][col1]-self.new_mat[row1][col2+1]+self.new_mat[row2+1][col2+1]
 
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
 # param_1 = obj.sumRegion(row1,col1,row2,col2)
+
+[[58, 44, 35, 28, 20, 0], 
+[48, 37, 28, 22, 18, 0], 
+[31, 25, 22, 19, 17, 0], 
+[22, 17, 16, 13, 12, 0], 
+[9,  8,  8,  5,  5,  0], 
+[0,  0,  0,  0,  0,  0]]
