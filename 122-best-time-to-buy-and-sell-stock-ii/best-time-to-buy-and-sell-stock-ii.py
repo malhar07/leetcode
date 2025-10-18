69b0e426c4ed163b49a644d233bd3165e2f53440
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-10-18 15:13:15 -0400

    Time: 1 ms (49.50%) | Memory: 18.8 MB (70.20%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,11 +1,10 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit = 0
-        min_p = 10001
-        for i in prices:
-            if i <= min_p:
-                min_p = i
-            else:
-                profit += i-min_p
-                min_p = i
-        return profit
+        stack = []
+        res = 0
+        for p in prices:
+            if stack and stack[-1] < p:
+                res += p-stack[-1]
+                stack.pop()
+            stack.append(p)
+        return res
