5b2807b9829b3ae41e59cf520bcb4f51a102975f
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-10-22 06:55:04 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (43.64%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -6,15 +6,12 @@ class Solution:
         write = m+n-1
         m-=1
         n-=1
-        
-
-        while m >= 0 and n >= 0:
-            if nums2[n] >= nums1[m]:
+        while n>=0:
+            if m<0 or nums1[m]<=nums2[n]:
                 nums1[write] = nums2[n]
                 n-=1
+                
             else:
                 nums1[write] = nums1[m]
                 m-=1
             write-=1
-        if n >= 0:
-            nums1[:write+1] = nums2[:n+1]
