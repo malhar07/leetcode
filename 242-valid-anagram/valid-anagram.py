7507180a4e6d3c08906447a85664451c89419071
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-10-21 20:20:34 -0400

    Time: 11 ms (71.98%) | Memory: 17.7 MB (97.42%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,14 +1,14 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        char_dict = [0]*26
-
-        for char in s:
-            char_dict[ord(char)-ord('a')] +=1
-        for char in t:
-            char_dict[ord(char)-ord('a')] -=1
-        print(char_dict)
-        for char_count in char_dict:
-            if char_count != 0:
+        char_count = [0]*26
+        if len(s) != len(t):
+            return False
+        for i in range(len(s)):
+            ind = ord(s[i]) - ord('a')
+            char_count[ind] += 1
+            ind2 = ord(t[i]) - ord('a')
+            char_count[ind2] -= 1
+        for i in char_count:
+            if i != 0:
                 return False
         return True
-         
