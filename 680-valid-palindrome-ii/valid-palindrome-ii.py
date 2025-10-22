475d594cb07be1155f092fcdb7abee0b0a1d3899
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-10-22 06:38:13 -0400

    Time: 55 ms (75.74%) | Memory: 18 MB (55.62%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,19 +1,21 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        def check_pal(s,left, right):
-            while left < right:
-                if s[left] != s[right]:
-                    return False
-                left+=1
-                right-=1
-            return True
-        
-        left = 0
-        right = len(s)-1
+        self.del_char = False
 
-        while left<right:
-            if s[left] != s[right]:
-                return check_pal(s, left, right-1) or check_pal(s, left+1, right)
-            left+=1
-            right-=1
-        return True
+        def check_pal(strng):
+            left = 0
+            right = len(strng)-1
+
+            while left < right:
+                if strng[left]!=strng[right]:
+                    if self.del_char == False:
+                        self.del_char = True
+                        return check_pal(strng[left:right]) or check_pal(strng[left+1:right+1])
+                    else:
+                        return False
+                left +=1
+                right -= 1
+            return True
+        return check_pal(s)
+
+                    
