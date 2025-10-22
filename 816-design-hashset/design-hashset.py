096c168f7996a45433ea4bb978b48d306e55c791
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-10-21 20:34:27 -0400

    Time: 49 ms (51.56%) | Memory: 44.6 MB (10.76%) - LeetSync

--- 816-design-hashset/design-hashset.py
+++ 816-design-hashset/design-hashset.py
@@ -1,37 +1,19 @@
-class ListNode:
-    def __init__(self, key):
-        self.key = key
-        self.next = None
 class MyHashSet:
 
     def __init__(self):
-        self.set = [ListNode(0) for i in range(10000)]
-
+        self.hashset = [False]*(10**6+1)
+        
 
     def add(self, key: int) -> None:
-
-        curr = self.set[key % len(self.set)]   
-        while curr.next:
-            if curr.next.key == key:
-                return 
-            curr = curr.next
-        curr.next = ListNode(key)
+        self.hashset[key] = True
+        
 
     def remove(self, key: int) -> None:
-        curr = self.set[key % len(self.set)]   
-        while curr.next:
-            if curr.next.key == key:
-                curr.next = curr.next.next
-                return
-            curr = curr.next
+        self.hashset[key] = False
+        
 
     def contains(self, key: int) -> bool:
-        curr = self.set[key % len(self.set)]   
-        while curr.next:
-            if curr.next.key == key:
-                return True
-            curr = curr.next
-        return False
+        return self.hashset[key]
         
 
 
