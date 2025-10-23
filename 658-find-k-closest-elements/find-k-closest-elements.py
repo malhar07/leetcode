26be537a7d521450d7274d82689a0baa3cdbc740
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-10-23 06:50:12 -0400

    Time: 16 ms (35.33%) | Memory: 19 MB (83.75%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.py
+++ 658-find-k-closest-elements/find-k-closest-elements.py
@@ -1,18 +1,14 @@
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        left = 0
-        right = k-1
-
-        for i in range(len(arr)-k):
-            if abs(arr[left] - x) <= abs(arr[right+1] - x):
-                if arr[left] == arr[right+1]:
-                    left+=1
-                    right+=1
-                else:
-                    break
-            # elif abs(arr[left] - x) == abs(arr[right+1] - x):
-
-            else:
-                left+=1
+        idx = len(arr)-1
+        while idx>0 and arr[idx] > x:
+            idx-=1
+        if idx+1<len(arr) and abs(arr[idx+1] - x) < abs(arr[idx] - x):
+            idx+=1
+        left = right = idx
+        while right-left+1 < k:
+            if left-1 < 0 or (right+1 < len(arr) and abs(arr[left-1] - x) > abs(arr[right+1] - x)):
                 right+=1
+            else:
+                left-=1
         return arr[left:right+1]
