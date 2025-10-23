a5174d2d0622a8b91c98c07e00c1d9de40cc9896
malhar07 <43933328+malhar07@users.noreply.github.com>
2025-10-22 20:29:40 -0400

    Time: 898 ms (18.44%) | Memory: 20.7 MB (37.05%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,27 +1,24 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-# [-4,-1,-1,0,1,2]
-        nums.sort()
+        # [-1,-1,0,1,2,4]
         res = []
-        print(nums)
-        for i in range(len(nums)-2):
-            if nums[i] > 0:
-                break
-            if i >0 and nums[i] == nums[i-1]:
+        nums.sort()
+
+        for ind, num in enumerate(nums):
+            if ind>0 and nums[ind] == nums[ind-1]:
                 continue
-            target = 0 - nums[i]
-            left = i+1
+            left = ind + 1
             right = len(nums)-1
             while left < right:
-                if nums[left] + nums[right] == target:
-                    res.append([nums[i],nums[left],nums[right]])
-                    while left<right and nums[left] == nums[left+1]:
-                        left+=1
-                    while right<left and nums[right] == nums[right-1]:
-                        right-=1
+                if nums[ind] + nums[left] + nums[right] == 0:
+                    res.append([nums[ind], nums[left], nums[right]])
                     left+=1
                     right-=1
-                elif nums[left] + nums[right] > target:
+                    while left < len(nums) and nums[left] == nums[left-1]:
+                        left+=1
+                    while right>=0 and nums[right] == nums[right+1]:
+                        right -= 1
+                elif nums[ind] + nums[left] + nums[right] > 0:
                     right-=1
                 else:
                     left += 1
