7d701c6c88f38d238694033a912495e998511089
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-01-20 22:03:22 -0500

    Time: 158 ms (62.26%) | Memory: 20.7 MB (23.20%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,19 +1,19 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+
+        def count_hours(speed):
+            return sum([math.ceil(x/speed) for x in piles])
         left = 1
         right = max(piles)
-        res = max(piles)
 
         while left <= right:
             mid = left + (right-left)//2
 
-            hours = sum([math.ceil(i/mid) for i in piles])
-            print(hours)
+            time = count_hours(mid)
 
-            if hours <= h:
-                res = min(mid, res)
+            if time <= h:
+                res = mid
                 right = mid-1
-                
-            else:
+            elif time > h:
                 left = mid+1
         return res
