08cef920fb869413d1805aef477d061743d64df2
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-01-25 17:42:48 -0500

    Time: 68 ms (44.54%) | Memory: 25.9 MB (7.03%) - LeetSync

--- 937-online-stock-span/online-stock-span.py
+++ 937-online-stock-span/online-stock-span.py
@@ -2,39 +2,24 @@ class StockSpanner:
 
     def __init__(self):
         self.stack = []
-        
+        self.ind = 0
 
     def next(self, price: int) -> int:
-        ans = 1
-        while self.stack and self.stack[-1][0] <= price:
-            ans+=self.stack.pop()[1]
-        self.stack.append((price,ans))
-        return ans
+        if not self.stack:
+            self.stack.append([price,1])
+            self.ind+=1
+            return 1
+        else:
+            res = 1
+            i = self.ind - 1
 
-        # if not self.stack:
-        #     self.stack.append((price,self.ind))
-        #     self.ind+=1
-        #     return 1
-        # else:
-        #     if self.stack[-1][0] > price:
-        #         self.stack.append((price,self.ind))
-        #         self.ind += 1
-        #         return 1
-        #     else:
-        #         while self.stack and price>= self.stack[-1][0]:
-        #             self.stack.pop()
-        #         if not self.stack:
-        #             res = self.ind+1
-        #         else:
-        #             res = self.ind - self.stack[-1][1]
-        #         self.stack.append((price,self.ind))
-        #         self.ind+=1
-
-        #         return res
-
+            while i >= 0 and self.stack[i][0] <= price:
+                res += self.stack[i][1]
+                i-= self.stack[i][1]
+            self.stack.append([price, res])
+            self.ind += 1
+            return res
 
 # Your StockSpanner object will be instantiated and called as such:
 # obj = StockSpanner()
 # param_1 = obj.next(price)
-[100,80,60,70,60,75]
-[1,1,1,2,1]
