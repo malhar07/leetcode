c852f0a127cbd490eb087dd0a7405b6d5e43c76d
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-06-08 22:35:41 -0400

    Time: 3 ms (60.89%) | Memory: 20.5 MB (20.67%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,10 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        stack = []
+        arr = [0]*len(prices)
+        arr[-1] = prices[-1]
         res = 0
-        for p in prices:
-            if stack and stack[-1] < p:
-                res += p-stack[-1]
-                stack.pop()
-            stack.append(p)
+        for i in range(len(prices)-2,-1,-1):
+            arr[i] = max(prices[i], arr[i+1])
+        
+        curr_max = arr[-1]
+        for i in range(len(prices)-2,-1,-1):
+            if prices[i] < curr_max:
+                res += curr_max - prices[i]
+            # else:
+            curr_max = prices[i]
         return res
