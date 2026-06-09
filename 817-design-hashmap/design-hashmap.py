e454f07dc087c0999d5cb874b95ea73fd8276199
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-06-08 21:42:41 -0400

    Time: 39 ms (64.48%) | Memory: 22.6 MB (92.55%) - LeetSync

--- 817-design-hashmap/design-hashmap.py
+++ 817-design-hashmap/design-hashmap.py
@@ -1,18 +1,43 @@
 class MyHashMap:
 
     def __init__(self):
-        self.map = [-1]*1000001
+        self.map = [-1]*1000
 
     def put(self, key: int, value: int) -> None:
-        self.map[key] = value
+        hash_val = key%1000
+        
+        if self.map[hash_val] == -1:
+            self.map[hash_val] = []
+
+        arr = self.map[hash_val]
+
+        for i in range(len(arr)):
+            k,v = arr[i]
+            if k == key:
+                arr[i] = (key,value)
+                return
+        self.map[hash_val].append((key,value))
+
+            
 
     def get(self, key: int) -> int:
-        return self.map[key]
-        
+        hash_val = key%1000
+        if self.map[hash_val] == -1:
+            return -1
+        else:
+            for hash_key,val in self.map[hash_val]:
+                if hash_key == key:
+                    return val
+        return -1
 
     def remove(self, key: int) -> None:
-        self.map[key] = -1
-
+        hash_val = key%1000
+        if self.map[hash_val] != -1:
+            for index, tup in enumerate(self.map[hash_val]):
+                hash_key, val = tup
+                if hash_key == key:
+                    self.map[hash_val].pop(index)
+                    return
 
 # Your MyHashMap object will be instantiated and called as such:
 # obj = MyHashMap()
