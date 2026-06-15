be37c433e8038999f31e6299b9180a288a24e5e2
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-06-15 16:37:49 -0400

    Time: 38 ms (72.57%) | Memory: 20.6 MB (13.23%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.py
+++ 572-subtree-of-another-tree/subtree-of-another-tree.py
@@ -6,31 +6,18 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        self.res = False
-        def check(root1, root2):
-            if not root1 and not root2:
+        def sameTree(node1, node2):
+            if not node1 and not node2:
                 return True
-            if not root1 or not root2:
+            if not node1 or not node2:
                 return False
-            if root1.val != root2.val:
+            if node1.val!=node2.val:
                 return False
-            return root1.val == root2.val and check(root1.left,root2.left) and check(root1.right,root2.right)
-        # return check(root.left, subRoot)
-        def helper(r, s):
-            # res = False
-            if not r:
-                return False
-            if r.val == s.val:
-                if check(r,s):
-                    # res = True
-                    return True
-            left = helper(r.left,s)
-            right = helper(r.right,s)
-            return left or right
+            return sameTree(node1.left, node2.left) and sameTree(node1.right, node2.right)
+        if not root:
+            return False
 
-        return helper(root, subRoot)
-        # return self.res
+        if sameTree(root, subRoot):
+            return True
 
-            
-
-             
+        return self.isSubtree(root.right, subRoot) or self.isSubtree(root.left, subRoot)
