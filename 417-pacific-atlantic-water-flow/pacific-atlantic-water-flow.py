faa206ea24684377898673db357110286c013f25
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-06-17 18:10:21 -0400

    Time: 35 ms (39.66%) | Memory: 21.1 MB (59.16%) - LeetSync

--- 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
+++ 417-pacific-atlantic-water-flow/pacific-atlantic-water-flow.py
@@ -1,35 +1,37 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        dirs = [[0,1], [1,0], [0,-1], [-1,0]]
-        pacific = deque()
-        atlantic = deque()
+        p_visited = set()
+        a_visited = set()
+        p_q = deque()
+        a_q = deque()
+        directions = [[0,1], [1,0], [0,-1], [-1,0]]
+        for r in range(len(heights)):
+            p_visited.add((r,0))
+            p_q.append((r,0))
+            a_visited.add((r,len(heights[0])-1))
+            a_q.append((r, len(heights[0])-1))
 
-        def bfs(que):
-            visited = set()
-            res = set()
+        for c in range(len(heights[0])):
+            p_visited.add((0,c))
+            p_q.append(((0,c)))
+            a_q.append((len(heights)-1, c))
+            a_visited.add((len(heights)-1, c))
 
-            while que:
-                r,c = que.popleft()
-                res.add((r,c))
-                visited.add((r,c))
+        
+        def bfs(q, ocean, visited):
+            while q:
+                r,c = q.popleft()
+                ocean.add((r,c))
 
-                for dr,dc in dirs:
+                for dr, dc in directions:
                     newr, newc = r+dr, c+dc
-                    if 0<=newr<len(heights) and 0<=newc<len(heights[0]) and heights[newr][newc] >= heights[r][c] and (newr,newc) not in visited:
-                        que.append((newr,newc))
+
+                    if 0<=newr<len(heights) and 0<=newc<len(heights[0]) and (newr,newc) not in visited and heights[newr][newc]>=heights[r][c]:
                         visited.add((newr,newc))
-            return res
+                        q.append((newr,newc))
+            return ocean
 
-        for i in range(len(heights)):
-            pacific.append((i,0))
-            atlantic.append((i, len(heights[0])-1))
-        for i in range(len(heights[0])):
-            pacific.append((0,i))
-            atlantic.append((len(heights)-1, i))
+        pacific = bfs(p_q, set(), p_visited)
+        atlantic = bfs(a_q, set(), a_visited)
+        return list(pacific.intersection(atlantic))
 
-        
-        pacific = bfs(pacific)
-        atlantic = bfs(atlantic)
-
-        return list(atlantic & pacific)
-        
