c0db17219ef6860bb9b0a30324be7e86fa5c8f3f
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-06-17 17:26:18 -0400

    Time: 3 ms (76.36%) | Memory: 19.2 MB (78.94%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.py
+++ 1036-rotting-oranges/rotting-oranges.py
@@ -1,35 +1,37 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         q = deque()
-        count = 0
-        time = 0
-        dirs = [[0,1],[1,0],[0,-1],[-1,0]]
-
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == 2:
-                    q.append((i,j))
-                if grid[i][j] == 1:
-                    count+=1
-        while q:
-            for i in range(len(q)):
-                r,c = q.popleft()
-
-                for dr, dc in dirs:
-                    newr, newc = r+dr, c+dc
-
-                    if 0<=newr<len(grid) and 0<=newc<len(grid[0]) and grid[newr][newc] == 1:
-                        grid[newr][newc] = 2
-                        q.append((newr,newc))
-                        count-=1
-            time+=1
-        if count > 0:
-            return -1
-        else:
-            if time == 0:
-                return time
-            else:
-                return time-1
+        visited = set()
+        directions = [[0,1], [1,0], [0,-1], [-1,0]]
+        fresh = False
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    fresh = True
+                if grid[r][c] == 2:
+                    q.append((r,c))
+        if len(q) == 0 and not fresh:
+            return 0
         
+        def bfs():
+            time = -1
+            while q:
+                for i in range(len(q)):
+                    r,c = q.popleft()
+                    for dr,dc in directions:
+                        newr, newc = r+dr, c+dc
+                        if 0<=newr<len(grid) and 0<=newc<len(grid[0]) and grid[newr][newc] == 1 and (newr, newc) not in visited:
+                            q.append((newr, newc))
+                            grid[newr][newc] = 2
+                            # visited.add((newr,newc))
+                time+=1
+
+            return time
 
 
+        time = bfs()
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    return -1
+        return time
