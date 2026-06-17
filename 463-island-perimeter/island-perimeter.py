9ccd6a7e4d9a238bdff89c6ad8826d92de158e7b
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-06-17 14:24:50 -0400

    Time: 49 ms (56.56%) | Memory: 19.6 MB (65.10%) - LeetSync

--- 463-island-perimeter/island-perimeter.py
+++ 463-island-perimeter/island-perimeter.py
@@ -1,38 +1,21 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        path = set()
-        row, col = len(grid), len(grid[0])
-        directions = [[0,1], [0,-1], [1,0], [-1,0]]
+        res = 0
+        def addPerimeter(r,c):
+            perimeter = 4
+            if r>0 and grid[r-1][c] == 1:
+                perimeter -= 1
+            if r<len(grid)-1 and grid[r+1][c]==1:
+                perimeter -= 1
+            if c >0 and grid[r][c-1] == 1:
+                perimeter -= 1
+            if c<len(grid[0])-1 and grid[r][c+1] == 1:
+                perimeter -= 1
+            return perimeter
 
-        # def bfs(row, col):
-        #     q = deque()
-        #     q.append((row,col))
-        #     perimeter = 0
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
 
-        #     path.add((row,col))
-
-        #     while q:
-        #         for i in range(len(q)):
-        #             r,c = q.popleft()
-        #             perimeter += 4
-
-        #             for dr, dc in directions:
-        #                 newr, newc = r+dr, c+dc
-        #                 if 0<= newr< len(grid) and 0<= newc<len(grid[0]) and grid[newr][newc] == 1 and(newr,newc) not in path:
-        #                     q.append((newr, newc))
-        #                     path.add((newr, newc))
-        #     return perimeter+2
-
-        
-        perimeter = 0
-        for i in range(row):
-            for j in range(col):
-                if grid[i][j] == 1:
-                    perimeter+=4
-                    for dr, dc in directions:
-                        newr, newc = i+dr, j+dc
-                        if 0<= newr< len(grid) and 0<= newc<len(grid[0]) and grid[newr][newc] == 1:
-                            perimeter -= 1
-
-
-        return perimeter
+                if grid[r][c] == 1:
+                    res += addPerimeter(r,c)
+        return res
