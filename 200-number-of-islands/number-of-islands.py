04b0ab312767d0fac73d5faf5de2cd7c8d9c9ea4
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-06-17 16:09:40 -0400

    Time: 249 ms (46.16%) | Memory: 21.6 MB (73.01%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,23 +1,23 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        res = 0
-        dirs = [[0,1], [1,0], [-1,0], [0,-1]]
-        visited = set()
-        
+        islands = 0
+
+        directions = [[0,1], [1,0], [0,-1], [-1,0]]
+
         def dfs(r,c):
-            visited.add((r,c))
+            if not 0<=r<len(grid) or not 0<= c < len(grid[0]):
+                return
+            grid[r][c] = "0"
 
-            for dr, dc in dirs:
-                newr, newc = r+dr, c+dc
+            for dr, dc in directions:
+                if 0<=r+dr<len(grid) and 0<=c+dc<len(grid[0]) and grid[r+dr][c+dc]=="1":
+                    dfs(r+dr, c+dc)
 
-                if 0<=newr<len(grid) and 0<=newc<len(grid[0]) and grid[newr][newc] == "1" and (newr,newc) not in visited:
-                    dfs(newr,newc)
 
-            return 
-        
         for r in range(len(grid)):
             for c in range(len(grid[0])):
-                if grid[r][c] == "1" and (r,c) not in visited:
+                if grid[r][c] == "1":
                     dfs(r,c)
-                    res+=1
-        return res
+                    islands+=1
+        
+        return islands
