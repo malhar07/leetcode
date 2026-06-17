740868b94692a413e8a5f2c27019270e444ba6b8
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-06-17 15:06:37 -0400

    Time: 0 ms (100.00%) | Memory: 19.4 MB (8.70%) - LeetSync

--- 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
@@ -1,24 +1,21 @@
 class Solution:
     def isAlienSorted(self, words: List[str], order: str) -> bool:
-        alpha_dict = {}
-        for i in range(len(order)):
-            alpha_dict[order[i]] = i
-        
-        def compare(char1, char2):
-            print(char1)
-            if alpha_dict[char1] <= alpha_dict[char2]:
-                return True
-            return False
-        
+        order_dict = {}
+        for index, char in enumerate(order):
+            order_dict[char] = index
         for i in range(1,len(words)):
-            ind = 0
-            while ind< len(words[i]) and ind < len(words[i-1]) and words[i][ind] == words[i-1][ind]:
-                ind+=1
-            if ind< len(words[i]) and ind < len(words[i-1]):
-                char1 = words[i-1][ind]
-                char2 = words[i][ind]
-                if not compare(char1, char2):
+            word1 = words[i-1]
+            word2 = words[i]
+            ind1, ind2 = 0,0
+
+            while ind1 < len(word1):
+                if ind2 >= len(word2):
                     return False
-            if ind == len(words[i]) and ind<len(words[i-1]):
-                return False
+                if word1[ind1] == word2[ind2]:
+                    ind1+=1
+                    ind2+=1
+                    continue
+                if order_dict[word1[ind1]] > order_dict[word2[ind2]]:
+                    return False
+                break
         return True
