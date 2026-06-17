d64c315bc6caaaeaf1cdd38d86f4bf8c9768585c
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-06-17 10:14:03 -0400

    Time: 0 ms (100.00%) | Memory: 19.6 MB (10.03%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -2,17 +2,21 @@ class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
 
-        def helper(arr, ind):
-            
+        def backtrack(ind, arr):
             if len(arr) == len(nums):
-                res.append(arr[:])
-                return 
-            if ind >= len(nums):
-                return 
+                res.append(arr.copy())
+                return
             
             for i in range(len(arr)+1):
-                left = arr[:i]
-                right = arr[i:]
-                helper(left + [nums[ind]] + right, ind+1)
-        helper([],0)
+                backtrack(ind+1, arr[:i]+[nums[ind]]+arr[i:])
+        backtrack(0,[])
         return res
+
+
+
+
+
+
+#                         [1,2]
+#     [3,1,2]             [1,3,2]             [1,2,3]
+#   [:i]+[3]+[i+:]
