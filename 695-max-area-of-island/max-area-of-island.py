b2763498b4396ed33c802e0ff6e4c57d11901344
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-06-17 16:39:04 -0400

    Time: 51 ms (5.74%) | Memory: 19.6 MB (84.45%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -5,24 +5,28 @@ class Solution:
 
         res = 0
 
-        def dfs(r,c) -> int:
-            visited.add((r,c))
+
+        def bfs(r,c) -> int:
+            q = deque()
+            q.append([r,c])
+            # visited.add((r,c))
             area = 0
-            for dr, dc in directions:
-                newr, newc = r+dr, c+dc
-                if 0<=newr<len(grid) and 0<=newc<len(grid[0]) and (newr,newc) not in visited and grid[newr][newc] == 1:
-                    # visited.add((newr,newc))
-                    area += dfs(newr,newc)
+            while q:
+                r,c = q.popleft()
+
+                for dr, dc in directions:
+                    newr, newc = r+dr, c+dc
+                    if 0<=newr<len(grid) and 0<=newc<len(grid[0]) and (newr,newc) not in visited and grid[newr][newc] == 1:
+                        visited.add((newr,newc))
+                        area += 1
+                        q.append((newr,newc))
             
-            return 1+area
+            return area+1
 
 
-
-            
-        
         for r in range(len(grid)):
             for c in range(len(grid[0])):
                 if grid[r][c] == 1:
-
-                    res = max(res, dfs(r,c))
+                    visited.add((r,c))
+                    res = max(res, bfs(r,c))
         return res
