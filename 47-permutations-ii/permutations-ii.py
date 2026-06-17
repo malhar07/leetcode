494af90e45b9826d3453fd3d6712bd396dbaf8f6
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-06-17 12:50:41 -0400

    Time: 6 ms (46.03%) | Memory: 19.9 MB (52.76%) - LeetSync

--- 47-permutations-ii/permutations-ii.py
+++ 47-permutations-ii/permutations-ii.py
@@ -1,28 +1,34 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        res = []
         nums.sort()
+        res = []
 
-        def helper(arr, used):
-            
+        def backtrack(ind, arr):
+            print(arr)
             if len(arr) == len(nums):
-                res.append(arr[:])
-                return 
-            # if ind >= len(nums):
-            #     return 
-            visited = set()
-            for i in range(len(nums)):
-                if not used[i] and nums[i] not in visited:
-                    used[i] = True
-                    visited.add(nums[i])
-                    arr.append(nums[i])
-                    helper(arr, used)
-                    arr.pop()
-                    used[i] = False
-                else:
-                    continue
-        
-        helper([],[False]*len(nums))
+                res.append(arr.copy())
+                return
+            start = 0
+
+            # If current number is a duplicate,
+            # insert it only after its existing copy.
+            if ind > 0 and nums[ind] == nums[ind - 1]:
+                last_index = -1
+
+                for i in range(len(arr)):
+                    if arr[i] == nums[ind]:
+                        last_index = i
+
+                start = last_index + 1
+
+            for i in range(start, len(arr)+1):
+                backtrack(ind+1, arr[:i] + [nums[ind]] + arr[i:])
+
+        backtrack(0,[])
         return res
-            
-        
+
+
+[1,1,2,2]
+    #     [1,2]
+    # [2,1,2] [1,2,2]
+
