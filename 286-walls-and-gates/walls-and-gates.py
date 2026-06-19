e058208913609dafde47fb70e890e8bef765343b
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-06-19 18:39:12 -0400

    Time: 43 ms (44.91%) | Memory: 25.9 MB (21.05%) - LeetSync

--- 286-walls-and-gates/walls-and-gates.py
+++ 286-walls-and-gates/walls-and-gates.py
@@ -3,28 +3,34 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
-        dirs = [[0,1], [1,0], [0,-1], [-1,0]]
+        visited = set()
+        directions = [[0,1],[1,0],[0,-1],[-1,0]]
+        rows, cols = len(rooms), len(rooms[0])
         q = deque()
 
-        for i in range(len(rooms)):
-            for j in range(len(rooms[0])):
-                if rooms[i][j] == 0:
-                    q.append((i,j))
+
+        for r in range(rows):
+            for c in range(cols):
+                if rooms[r][c] == 0:
+                    q.append((r,c))
+                    visited.add((r,c))
         
-        def bfs():
-            visited = set()
-            curr_distance = 1
+
+        def bfs(q):
+            steps = 0
 
             while q:
-                for _ in range(len(q)):
-                    r,c = q.popleft()
+                
+                # visited.add((r,c))
 
-                    for dr, dc in dirs:
+                for i in range(len(q)):
+                    r,c = q.popleft()
+                    rooms[r][c] = steps
+                    for dr, dc in directions:
                         newr, newc = r+dr, c+dc
-                        if 0<=newr<len(rooms) and 0<=newc<len(rooms[0]) and rooms[newr][newc] > 0 and (newr,newc) not in visited:
-                            rooms[newr][newc] = curr_distance
-                            visited.add((newr,newc))
+
+                        if 0<=newr<rows and 0<=newc<cols and (newr,newc) not in visited and rooms[newr][newc]  == 2147483647:
                             q.append((newr,newc))
-                curr_distance += 1
-        bfs()
-        # return 
+                            visited.add((newr,newc))
+                steps += 1
+        bfs(q)
