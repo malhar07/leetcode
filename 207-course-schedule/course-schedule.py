0046685cce0cb005482384dbb9cb116b8dfb3278
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-06-19 16:46:48 -0400

    Time: 8 ms (22.48%) | Memory: 21.3 MB (26.20%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,31 +1,41 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        prereq = {crs:[] for crs in range(numCourses)}
-        for crs, pre in prerequisites:
-            prereq[crs].append(pre)
-        path = set()
-        memo = set()
-        
+        prereq = {i:[] for i in range(numCourses)}
+        for course, pre in prerequisites:
+            prereq[course].append(pre)
+        visited = set()
         def dfs(crs):
-            if crs in path:
+            if crs in visited:
                 return False
-            if crs in memo:
+            if prereq[crs] == []:
                 return True
-            path.add(crs)
-
-            for pre in prereq[crs]:
-                if not dfs(pre):
-                    return False
-            memo.add(crs)
-            path.remove(crs)
-            return True
             
+            visited.add(crs)
+
+            for nei in prereq[crs]:
+                if not dfs(nei):
+                    return False
+
+            visited.remove(crs)
+
+            prereq[crs] = []
+            return True
         for crs in range(numCourses):
             if not dfs(crs):
                 return False
         return True
+        
 
-        # 0: 1,2,3
-        # 1:2,3
-        # 2:3
-        # 3:0
+
+
+
+
+
+
+
+
+
+        0-1
+        1-2
+        2-4
+        4-5
