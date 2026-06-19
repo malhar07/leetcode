529690bba8fed6f8be876bccec6223678ed7dea3
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-06-19 18:28:18 -0400

    Time: 515 ms (34.00%) | Memory: 43.2 MB (53.47%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.py
+++ 2035-count-sub-islands/count-sub-islands.py
@@ -1,49 +1,34 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-        dirs = [[0,1], [1,0], [0,-1], [-1,0]]
         visited = set()
-        def dfs(r,c):
-            visited.add((r,c))
-            found = True
-            if grid1[r][c] != 1:
-                found = False
-            # found = grid1[r][c] == 1
+        directions = [[0,1], [1,0], [0,-1], [-1,0]]
+        rows, cols = len(grid2), len(grid2[0])
+        subIslands = 0
 
-            for dr, dc in dirs:
-                newr, newc = r+dr, c+dc
-                if 0<=newr<len(grid2) and 0<=newc<len(grid2[0]) and (newr,newc) not in visited and grid2[newr][newc] == 1:
-                    # visited.add((newr,newc))
-                    found =  dfs(newr,newc) and found
-            return found
+        def bfs(row,col):
+            q = deque()
+            q.append((row,col))
+            valid = True
 
-        count = 0
-        for i in range(len(grid2)):
-            for j in range(len(grid2[0])):
-                if grid2[i][j] == 1 and (i,j) not in visited:
-                    
-                    if dfs(i,j):
-                        print(i, " ", j)
-                        count+=1
-        return count
+            while q:
+                (r,c) = q.popleft()   
+                if grid1[r][c] != 1:
+                    valid = False
+                
+                for dr, dc in directions:
+                    newr, newc = r+dr, c+dc
 
-# class Solution:
-#     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
-#         dirs = [(0,1), (1,0), (-1,0), (0,-1)]
-#         visited = set()
+                    if 0<=newr<rows and 0<=newc<cols and grid2[newr][newc] == 1 and (newr, newc) not in visited:
+                        q.append((newr, newc))
+                        visited.add((newr, newc))
+            if not valid:
+                return 0
+            return 1
 
-#         def dfs(r, c):
-#             visited.add((r, c))
-#             is_sub = grid1[r][c] == 1
-#             for dr, dc in dirs:
-#                 nr, nc = r + dr, c + dc
-#                 if 0 <= nr < len(grid2) and 0 <= nc < len(grid2[0]) and grid2[nr][nc] == 1 and (nr, nc) not in visited:
-#                     is_sub = dfs(nr, nc) and is_sub
-#             return is_sub
 
-#         count = 0
-#         for i in range(len(grid2)):
-#             for j in range(len(grid2[0])):
-#                 if grid2[i][j] == 1 and (i, j) not in visited:
-#                     if dfs(i, j):
-#                         count += 1
-#         return count
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) not in visited and grid2[r][c] == 1:
+                    visited.add((r,c))
+                    subIslands += bfs(r,c)
+        return subIslands
