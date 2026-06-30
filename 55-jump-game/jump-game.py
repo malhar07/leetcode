eae3e14216808111579041518aa890eb03ee2778
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-06-30 17:00:12 -0400

    Time: 15 ms (80.07%) | Memory: 20.3 MB (48.15%) - LeetSync

--- 55-jump-game/jump-game.py
+++ 55-jump-game/jump-game.py
@@ -1,8 +1,15 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        curr = len(nums)-1
-
+        # reachable = set(len(nums)-1)
+        pos = len(nums)-1
+        target = len(nums)-1
         for i in range(len(nums)-2,-1,-1):
-            if i + nums[i] >= curr:
-                curr = i
-        return curr == 0
+
+            curr_jump = nums[i]
+            curr_index = i
+
+            if curr_index + curr_jump >= target:
+                target = curr_index
+        return target == 0
+
+
