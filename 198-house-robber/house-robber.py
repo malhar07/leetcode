80f9fd0a7d756681a9974db29242e1409752c9e7
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-07-06 18:15:31 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (89.88%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,17 +1,29 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        n = len(nums)
-        memo = [-1] * len(nums)
+        # n = len(nums)
+        # memo = [-1] * len(nums)
 
-        def dfs(ind):
-            if ind>=n:
-                return 0
-            if memo[ind] != -1:
-                return memo[ind]
-            rob_curr = nums[ind] + dfs(ind+2)
-            skip = dfs(ind+1)
-            if memo[ind] == -1:
-                memo[ind] = max(skip, rob_curr)
-            return memo[ind]
-        return dfs(0)
+        # def dfs(ind):
+        #     if ind>=n:
+        #         return 0
+        #     if memo[ind] != -1:
+        #         return memo[ind]
+        #     rob_curr = nums[ind] + dfs(ind+2)
+        #     skip = dfs(ind+1)
+        #     if memo[ind] == -1:
+        #         memo[ind] = max(skip, rob_curr)
+        #     return memo[ind]
+        # return dfs(0)
+
+        memo = [0] * (len(nums) + 2)
+
+
+        for i in range(len(nums)-1,-1,-1):
+            curr = max((nums[i] + memo[i+2]), memo[i+1])
+            memo[i] = curr
+        return memo[0]
+
+
+
+
             
