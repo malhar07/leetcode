cf2058e232108f5a4350d3e113cd47c8b9a492fe
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-07-06 10:27:10 -0400

    Time: 283 ms (5.03%) | Memory: 51.7 MB (55.73%) - LeetSync

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -3,26 +3,25 @@ class Solution:
         con = set()
         adjlist = defaultdict(list)
         res = 0
-        visited = set()
 
         for n1, n2 in connections:
-            adjlist[n1].append(n2)
-            adjlist[n2].append(n1)
+            adjlist[n1].append([n2,1])
+            adjlist[n2].append([n1,0])
 
             con.add((n1,n2))
         
         q = deque([(0,-1)]) #curr, parent
-        visited.add(0)
+
 
         while q:
             curr, parent = q.popleft()
 
-            for nei in adjlist[curr]:
+            for nei, cost in adjlist[curr]:
                 if nei == parent:
                     continue
-                if (curr,nei) in con:
-                    res+=1
-                visited.add(nei)
+                # if (curr,nei) in con:
+                res+=cost
+                # visited.add(nei)
                 q.append((nei, curr))
         return res
                 
