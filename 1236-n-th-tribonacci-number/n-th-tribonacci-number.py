61374fc432d845f054f6018c075cf4df8a6c69e7
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-07-06 17:54:48 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (53.61%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -1,12 +1,18 @@
 class Solution:
     def tribonacci(self, n: int) -> int:
-        if n == 0:
+        one = 0
+        two = 1
+        three = 1
+
+        if n==0:
             return 0
-        if n == 1 or n == 2:
+        if n<=2:
             return 1
-        tribo = [0,1,1]
         for i in range(3,n+1):
-            curr = sum(tribo)
-            tribo[0], tribo[1] = tribo[1], tribo[2]
-            tribo[2] = curr
-        return tribo[2]
+            curr = one+two+three
+            
+            
+            one = two
+            two = three
+            three = curr
+        return curr
