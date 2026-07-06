360dfb4ca7fdca1c14f4bbc4a99de2ba89dd511b
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-07-06 11:20:39 -0400

    Time: 24 ms (41.55%) | Memory: 19.4 MB (50.51%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.py
+++ 945-snakes-and-ladders/snakes-and-ladders.py
@@ -1,66 +1,37 @@
 class Solution:
     def snakesAndLadders(self, board: List[List[int]]) -> int:
         n = len(board)
-        board = board[::-1]
-        for ind, row in enumerate(board):
-            if ind%2 == 1:
-                board[ind] = row[::-1][:]
-        visited = set([0])
-        q = deque([(0)])
+        visited = set()
+        def getPos(num):
+            row = (n-1) - ((num-1) // n)
+            col = (num-1) % n
+
+            if ((num-1)//n + 2) % 2 != 0:
+                col = (n-1) - col
+            return (row,col)
+        
         def bfs():
-            turns = 0
+            q = deque([1])
+            visited.add(1)
+            rolls = 1
             while q:
-                for _ in range(len(q)):
-                    curr = q.popleft()
-                    if curr >= n**2-1:
-                        return turns
+
+                for i in range(len(q)):
+                    pos = q.popleft()
 
                     for die in range(1,7):
-                        new_pos = curr+die
-                        # if new_pos not in visited:
-                        # visited.add(new_pos)
-                        r,c = new_pos//n, new_pos%n
-                        if 0<=r<n and 0<=c<n and board[r][c]!=-1:
-                            new_pos = board[r][c]-1
-                        if new_pos not in visited:
-                            q.append(new_pos)
-                            visited.add(new_pos)
-                turns+=1
-            return -1    
-        return bfs()
-from collections import deque
-
-class Solution:
-    def snakesAndLadders(self, board: List[List[int]]) -> int:
-        n = len(board)
-        board = board[::-1]
-        for i, row in enumerate(board):
-            if i % 2 == 1:
-                board[i] = row[::-1][:]
-
-        visited = set([0])
-        q = deque([0])
-
-        def bfs():
-            turns = 0
-            while q:
-                for _ in range(len(q)):
-                    curr = q.popleft()
-                    if curr == n * n - 1:
-                        return turns
-
-                    for die in range(1, 7):
-                        new_pos = curr + die
-                        if new_pos >= n * n:
+                        next_pos = pos+die
+                        if next_pos > n**2:
                             continue
-                        r, c = new_pos // n, new_pos % n
-                        if board[r][c] != -1:
-                            new_pos = board[r][c] - 1
-
-                        if new_pos not in visited:
-                            visited.add(new_pos)
-                            q.append(new_pos)
-                turns += 1
+                        row, col = getPos(next_pos)
+                        if board[row][col] != -1:
+                            next_pos =  board[row][col]
+                        if next_pos == n**2:
+                            return rolls
+                        if next_pos in visited:
+                            continue
+                        q.append(next_pos)
+                        visited.add(next_pos)
+                rolls += 1
             return -1
-
         return bfs()
