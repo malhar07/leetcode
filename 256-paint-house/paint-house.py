5f97f3ceb8e1633246aafc065c9374d7c6c14fed
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-07-06 18:40:05 -0400

    Time: 4 ms (42.16%) | Memory: 19.3 MB (81.51%) - LeetSync

--- 256-paint-house/paint-house.py
+++ 256-paint-house/paint-house.py
@@ -1,21 +1,33 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
-        n = len(costs)
-        memo = {}
-        def dfs(color, ind):
-            red = blue = green = math.inf
-            if ind >= n:
-                return 0
-            if (color, ind) in memo:
-                return memo[(color, ind)]
+        # n = len(costs)
+        # memo = {}
+        # def dfs(color, ind):
+        #     red = blue = green = math.inf
+        #     if ind >= n:
+        #         return 0
+        #     if (color, ind) in memo:
+        #         return memo[(color, ind)]
             
-            if color != 0:
-                red = costs[ind][0] + dfs(0, ind+1)
-            if color != 1:
-                blue = costs[ind][1] + dfs(1, ind+1)
-            if color != 2:
-                green = costs[ind][2] + dfs(2, ind+1)
-            if (color, ind) not in memo:
-                memo[(color, ind)] = min(red, blue, green)
-            return memo[(color, ind)]
-        return dfs(-1, 0)
+        #     if color != 0:
+        #         red = costs[ind][0] + dfs(0, ind+1)
+        #     if color != 1:
+        #         blue = costs[ind][1] + dfs(1, ind+1)
+        #     if color != 2:
+        #         green = costs[ind][2] + dfs(2, ind+1)
+        #     if (color, ind) not in memo:
+        #         memo[(color, ind)] = min(red, blue, green)
+        #     return memo[(color, ind)]
+
+        # return dfs(-1, 0)
+
+
+        memo = costs[-1]
+        for i in range(len(costs)-2,-1,-1):
+            red = costs[i][0] + min(memo[1], memo[2])
+            blue = costs[i][1] + min(memo[0], memo[2])
+            green = costs[i][2] + min(memo[0], memo[1])
+
+            memo = [red, blue, green]
+            print(memo)
+        return min(memo)
