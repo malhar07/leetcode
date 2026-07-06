0ff8043159a9d35de5a3d5956aaf1d6073c8fd21
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-07-06 15:26:35 -0400

    Time: 160 ms (58.61%) | Memory: 19.8 MB (61.12%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.py
@@ -1,23 +1,34 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+
+        def checkValidCell(row, col):
+            if row<0 or row>=n:
+                return False
+            if col < 0 or col>=n:
+                return False
+            if grid[row][col] == 1:
+                return False
+            return True
+
         n = len(grid)
-        q = deque()
-        visited = set()
-
-        if grid[0][0]!= 0 or grid[n-1][n-1]!=0:
+        directions = [[0,1], [0,-1], [-1,-1], [-1,0], [-1,1], [1,-1], [1,0], [1,1]]
+        res = 1
+        
+        if grid[0][0] == 1 or grid[n-1][n-1] == 1:
             return -1
+        q = deque([(0,0,1)])
+        grid[0][0] = 1
 
-        q.append((0,0,1))
-        visited.add((0,0))
-        directions = [[0,1], [0,-1], [1,0], [-1,0], [1,1], [1,-1], [-1,1], [-1,-1]]
         while q:
-            r,c,path_len = q.popleft()
+            r,c,count = q.popleft()
+            
             if r == n-1 and c == n-1:
-                return path_len
+                    return count
 
             for dr, dc in directions:
                 newr, newc = r+dr, c+dc
-                if 0<=newr<n and 0<=newc<n and (newr,newc) not in visited and grid[newr][newc]==0:
-                    visited.add((newr,newc))
-                    q.append((newr,newc,path_len+1))
+                if checkValidCell(newr, newc):
+                    q.append((newr,newc,count+1))
+                    grid[newr][newc] = 1
+
         return -1
