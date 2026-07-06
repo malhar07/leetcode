77c37906768937b527652f41c94fe0687eddecac
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-07-06 18:33:18 -0400

    Time: 11 ms (11.62%) | Memory: 21.4 MB (14.15%) - LeetSync

--- 256-paint-house/paint-house.py
+++ 256-paint-house/paint-house.py
@@ -1,17 +1,21 @@
 class Solution:
     def minCost(self, costs: List[List[int]]) -> int:
+        n = len(costs)
         memo = {}
-        def dfs(prev,ind):
-            if ind == len(costs):
+        def dfs(color, ind):
+            red = blue = green = math.inf
+            if ind >= n:
                 return 0
-            if (prev,ind) not in memo:
-                min_cost = math.inf
-                for i, cost in enumerate(costs[ind]):
-                    if i == prev:
-                        continue
-                    curr = cost + dfs(i, ind+1)
-                    min_cost = min(min_cost, curr)
-                memo[(prev,ind)] = min_cost
-            return memo[(prev,ind)]
-        
-        return dfs(3,0)
+            if (color, ind) in memo:
+                return memo[(color, ind)]
+            
+            if color != 0:
+                red = costs[ind][0] + dfs(0, ind+1)
+            if color != 1:
+                blue = costs[ind][1] + dfs(1, ind+1)
+            if color != 2:
+                green = costs[ind][2] + dfs(2, ind+1)
+            if (color, ind) not in memo:
+                memo[(color, ind)] = min(red, blue, green)
+            return memo[(color, ind)]
+        return dfs(-1, 0)
