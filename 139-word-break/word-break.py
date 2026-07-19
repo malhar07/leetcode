4834ae1b69292691aca9e030059f512e6fe2195a
malhar07 <43933328+malhar07@users.noreply.github.com>
2026-07-18 20:13:07 -0400

    Time: 3 ms (72.62%) | Memory: 19.8 MB (7.45%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -1,16 +1,23 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        memo = {len(s):True}
+        wordset = set(wordDict)
+        memo = {}
 
-        memo[-1] = True
-        def helper(ind):
+        def backtrack(ind):
+            if ind >= len(s):
+                return True
             if ind in memo:
                 return memo[ind]
-            found = False
+
             for i in range(ind, len(s)):
-                if s[ind:i+1] in wordDict:
-                    if helper(i+1):
-                        found = True
-            memo[ind] = found
-            return memo[ind]
-        return helper(0)
+                curr = s[ind:i+1]
+                if curr in wordset:
+                    print(curr)
+                    if backtrack(i+1):
+                        return True
+                    else:
+                        memo[i+1] = False
+            memo[ind] = False
+            return False
+
+        return backtrack(0)          
